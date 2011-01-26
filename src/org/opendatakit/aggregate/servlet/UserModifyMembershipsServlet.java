398b76e3c933e9f43bede13f342229ce85c36fe0
msundt <msundt@cs.washington.edu>
2011-01-26 15:43:10 -0800

    Clear the cached application-layer User objects when group memberships change (will still need to log out, clear cookie, log in).

--- src/org/opendatakit/aggregate/servlet/UserModifyMembershipsServlet.java
+++ src/org/opendatakit/aggregate/servlet/UserModifyMembershipsServlet.java
@@ -244,6 +244,8 @@ public class UserModifyMembershipsServlet extends ServletUtilBase {
 			resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
 					ErrorConsts.PERSISTENCE_LAYER_PROBLEM);
 			return;
+		} finally {
+			cc.getUserService().reloadPermissions();
 		}
 
 		Map<String,String> properties = new HashMap<String,String>();
