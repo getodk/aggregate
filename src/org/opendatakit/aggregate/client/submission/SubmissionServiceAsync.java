fa95d08e02a6e139c46d804331f3c906b62d468f
wbrunette <devnull@localhost>
2011-03-04 20:56:33 -0800

    Add the beginnings of the submission service

---
+++ src/org/opendatakit/aggregate/client/submission/SubmissionServiceAsync.java
@@ -0,0 +1,11 @@
+package org.opendatakit.aggregate.client.submission;
+
+import org.opendatakit.aggregate.client.filter.FilterGroup;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface SubmissionServiceAsync {
+
+  void getSubmissions(FilterGroup filter, AsyncCallback<SubmissionSummaryBatch> callback);
+
+}
