9c8fe917fee4ab87dcb8610494a4da25df77c818
ngarrett 21 <ngarrett.21@gmail.com>
2011-03-05 19:05:06 -0800

    Prettier Tabbing

--- war/javascript/resize.js
+++ war/javascript/resize.js
@@ -3,15 +3,15 @@ var MAX_SIDE_PANEL_WIDTH = 200;
 function onWindowResize() {
 	var height = $(window).height();
 	var width = $(window).width();
-	$(".mainNav").width(width - 6);
+	$(".mainNav").width(width);
 	$(".mainNav").height(height - 26);
-	$("#spacer_tab").width(width - 300);
+	$("#main_nav_spacer_tab").width(width - 300);
 	
 	var sidePanelPotentialWidth = width * .15;
 	var sidePanelActualWidth = Math.min(MAX_SIDE_PANEL_WIDTH, sidePanelPotentialWidth);
 	$(".filters_panel").width(sidePanelActualWidth);
 	$(".help_panel").width(sidePanelActualWidth);
-	$("#data_table").width(width - 22 - (sidePanelActualWidth * 2));
+	$("#data_table").width(width - 16 - (sidePanelActualWidth * 2));
 }
 
 function gwtContentLoaded() {
