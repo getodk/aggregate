7e5c012552b0c1dc2b732ce178753a782ff1c17d
ngarrett 21 <ngarrett.21@gmail.com>
2011-03-08 03:03:57 -0800

    Restyled Tabbing, got rid of url parameters in favor of url hash, unbroke resizing.

--- src/org/opendatakit/aggregate/client/filter/CreateNewFilterPopup.java
+++ src/org/opendatakit/aggregate/client/filter/CreateNewFilterPopup.java
@@ -62,6 +62,7 @@ public class CreateNewFilterPopup extends PopupPanel{
 		
 		//on exit
 		final Button submit = new Button("Save Filter");
+		submit.addStyleDependentName("positive");
 		submit.addClickHandler(new ClickHandler() {
 
 			@Override
@@ -114,6 +115,7 @@ public class CreateNewFilterPopup extends PopupPanel{
 		});
 		
 		Button exit = new Button("Cancel");
+		exit.addStyleDependentName("negative");
 		exit.addClickHandler(new ClickHandler() {
 
 			@Override
--- war/javascript/resize.js
+++ war/javascript/resize.js
@@ -3,16 +3,12 @@ var MAX_SIDE_PANEL_WIDTH = 200;
 // Handle resizing various parts of the page based on
 // the screen size.
 function onWindowResize() {
-	var height = $(window).height();
 	var width = $(window).width();
 	$(".mainNav").width(width);
-	$(".mainNav").height(height - 26);
-	$("#main_nav_spacer_tab").width(width - 400);
-	$("#manage_nav_spacer_tab").width(width - 400);
 	
 	var sidePanelPotentialWidth = width * .15;
 	var sidePanelActualWidth = Math.min(MAX_SIDE_PANEL_WIDTH, sidePanelPotentialWidth);
-	$(".filters_panel").width(sidePanelActualWidth);
-	$(".help_panel").width(sidePanelActualWidth);
-	$("#data_table").width(width - 16 - (sidePanelActualWidth * 2));
+	$("#filters_panel").width(sidePanelActualWidth);
+	$("#help_panel").width(sidePanelActualWidth);
+	$("#filters_data_help").width(width);
 }
