86760a06ca9b3f76964400290fbe7ed9c6616191
kyle <devnull@localhost>
2011-03-13 13:59:18 -0700

    refactor of aggregate ui

--- src/org/opendatakit/aggregate/client/AggregateUI.java
+++ src/org/opendatakit/aggregate/client/AggregateUI.java
@@ -5,15 +5,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.opendatakit.aggregate.client.filter.ColumnFilter;
-import org.opendatakit.aggregate.client.filter.ColumnFilterHeader;
-import org.opendatakit.aggregate.client.filter.CreateNewFilterPopup;
 import org.opendatakit.aggregate.client.filter.Filter;
 import org.opendatakit.aggregate.client.filter.FilterGroup;
 import org.opendatakit.aggregate.client.filter.FilterService;
 import org.opendatakit.aggregate.client.filter.FilterServiceAsync;
 import org.opendatakit.aggregate.client.filter.FilterSet;
-import org.opendatakit.aggregate.client.filter.RowFilter;
 import org.opendatakit.aggregate.client.form.FormService;
 import org.opendatakit.aggregate.client.form.FormServiceAsync;
 import org.opendatakit.aggregate.client.form.FormSummary;
@@ -29,63 +25,36 @@ import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.logical.shared.CloseEvent;
-import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.user.client.Timer;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DecoratedTabPanel;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TabPanel;
-import com.google.gwt.user.client.ui.Tree;
-import com.google.gwt.user.client.ui.TreeItem;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class AggregateUI implements EntryPoint {
   
   private static final int REFRESH_INTERVAL = 5000; // ms
   
   // Main Navigation
-  private static final String SUBMISSIONS = "submissions";
-  private static final String MANAGEMENT = "management";
-  private static final String[] MAIN_MENU = {SUBMISSIONS, MANAGEMENT};
-  // Submission Navigation
-  private static final String FILTER = "filter";
-  private static final String VISUALIZE = "visualize";
-  private static final String[] SUBMISSION_MENU = {FILTER, VISUALIZE};
-  // Management Navigation
-  private static final String FORMS = "forms";
-  private static final String EXPORT = "export";
-  private static final String PERMISSIONS = "permissions";
-  private static final String UTILITIES = "utilities";
-  private static final String[] MANAGEMENT_MENU = {FORMS, EXPORT, PERMISSIONS, UTILITIES};
-
+  private static final String[] MAIN_MENU = {
+	  SubmissionTabUI.SUBMISSIONS, ManageTabUI.MANAGEMENT};
+  
   private List<FilterGroup> view = new ArrayList<FilterGroup>();
-  private FlexTable dataTable; //contains the data
+  private FlexTable dataTable = new FlexTable(); //contains the data
   private FilterGroup def; //the default filter group
-  private HorizontalPanel filterPanel = new HorizontalPanel();
-  private CreateNewFilterPopup filterPopup = new CreateNewFilterPopup();
   private UrlHash hash;
   
   // navigation
   private DecoratedTabPanel mainNav = new DecoratedTabPanel();
-  private TabPanel manageNav = new TabPanel();
-  private TabPanel submissionNav = new TabPanel();
+  private ManageTabUI manageNav;
+  private SubmissionTabUI submissionNav;
   
   // Report tab
-  private HorizontalPanel filtersDataHelp = new HorizontalPanel();
-  private FlexTable formAndGoalSelectionTable = new FlexTable();
-  private FlexTable uploadTable = new FlexTable();
-
   private FormServiceAsync formSvc;
   private SubmissionServiceAsync submissionSvc;
   private FilterServiceAsync filterSvc;
@@ -108,141 +77,6 @@ public class AggregateUI implements EntryPoint {
     };
     refreshTimer.scheduleRepeating(REFRESH_INTERVAL);
     
-  }
-  
-  public HorizontalPanel setupFormsAndGoalsPanel() {
-    // list of forms
-    formAndGoalSelectionTable.setWidget(0, 0, formsBox);
-    // list of filters
-    formAndGoalSelectionTable.setWidget(0, 1, filtersBox);
-    // load form + filter
-    Button loadFormAndFilterButton = new Button("Load Filter");
-    formAndGoalSelectionTable.setWidget(0, 2, loadFormAndFilterButton);
-    formAndGoalSelectionTable.setHTML(0, 3, "&nbsp;&nbsp;");
-
-    // end goals vis, export, publish
-    Button exportButton = new Button("<img src=\"images/green_right_arrow.png\" /> Export");
-    formAndGoalSelectionTable.setWidget(0, 4, exportButton);
-    Button publishButton = new Button("<img src=\"images/green_right_arrow.png\" /> Publish");
-    formAndGoalSelectionTable.setWidget(0, 5, publishButton);
-
-    HorizontalPanel formsAndGoalsPanel = new HorizontalPanel();
-    formsAndGoalsPanel.add(formAndGoalSelectionTable);
-    formsAndGoalsPanel.getElement().setId("form_and_goals_panel");
-    return formsAndGoalsPanel;
-  }
-
-  public TreeItem loadFilterGroup(final FilterGroup group) {
-	  TreeItem filterGroup = new TreeItem(
-			  new Label(group.getName()));
-	  
-	  final FlexTable filters = new FlexTable();
-	  
-	  int row = 0;
-	  for (Filter filter: group.getFilters()) {
-		  if(filter instanceof RowFilter) {
-			  RowFilter rowFilter = (RowFilter) filter;
-			  filters.setWidget(row, 0, new Label(
-					  rowFilter.getVisibility() + rowFilter.getColumn().getDisplayHeader() + 
-					  "where columns are " + rowFilter.getOperation() + 
-					  rowFilter.getInput()));
-		  } else if (filter instanceof ColumnFilter){
-			  ColumnFilter columnFilter = (ColumnFilter) filter;
-			  List<ColumnFilterHeader> columns = columnFilter.getColumnFilterHeaders();
-			  String columnNames = "";
-			  for(ColumnFilterHeader column: columns) {
-				  columnNames += " " + column.getColumn().getDisplayHeader();
-			  }
-			  filters.setWidget(row, 0, new Label(
-					  columnFilter.getVisibility() + columnNames));
-		  }
-		  final Button removeFilter = new Button("-");
-		  filters.setWidget(row, 1, removeFilter);
-		  removeFilter.getElement().setPropertyObject(
-				  "filter", filter);
-		  removeFilter.addClickHandler(new ClickHandler() {
-
-			@Override
-			public void onClick(ClickEvent event) {
-				Filter remove = (Filter)removeFilter.getElement()
-					.getPropertyObject("filter");
-				group.removeFilter(remove);
-				filterPanel.clear();
-				setupFiltersDataHelpPanel(view);
-			} 
-		  });
-		  row++;
-	  }
-	  filterGroup.addItem(filters);
-	  filterGroup.setState(true);
-	  return filterGroup;
-  }
-  
-  public HorizontalPanel setupFiltersDataHelpPanel(
-		  List<FilterGroup> groups) {
-	  //create filter tree
-	  Tree activeFilters = new Tree();
-	  TreeItem title = new TreeItem(new Label("Active Filters"));
-	  activeFilters.addItem(title);
-	  
-	  for (FilterGroup group : groups) {
-		  TreeItem itemGroup = loadFilterGroup(group);
-		  title.addItem(itemGroup);
-		  title.setState(true);
-	  }
-	  
-	  //add new filter button
-	  Button newFilter = new Button();
-	  newFilter.setHTML("<img src=\"images/yellow_plus.png\" /> New Filter");
-	  newFilter.addClickHandler(new ClickHandler(){
-
-		@Override
-		public void onClick(ClickEvent event) {
-			filterPopup = new CreateNewFilterPopup(dataTable, def);
-			filterPopup.setPopupPositionAndShow(
-				new PopupPanel.PositionCallback() {
-					
-					@Override
-					public void setPosition(int offsetWidth, int offsetHeight) {
-						int left = (Window.getClientWidth() - offsetWidth) / 2;
-						int top = (Window.getClientHeight() - offsetHeight) / 2;
-						filterPopup.setPopupPosition(left, top);
-					}
-				});
-		  filterPopup.addCloseHandler(new CloseHandler<PopupPanel>() {
-			  
-			  @Override
-			  public void onClose(CloseEvent<PopupPanel> event) {
-				  filterPanel.clear();
-				  setupFiltersDataHelpPanel(view);
-			  }
-			
-		  });
-		}
-	  });
-	  
-	  activeFilters.add(newFilter);
-	  filtersDataHelp.add(activeFilters);
-
-	  
-    // view data
-    dataTable = new FlexTable();
-    dataTable.getRowFormatter().addStyleName(0, "titleBar");
-    dataTable.addStyleName("dataTable");
-    filtersDataHelp.add(dataTable);
-
-    // view help
-    VerticalPanel helpPanel = new VerticalPanel();
-    for (int i = 1; i < 5; i++) {
-      helpPanel.add(new HTML("Help Content " + i));
-    }
-    helpPanel.setStyleName("help_panel");
-    filtersDataHelp.add(helpPanel);
-    filtersDataHelp.getElement().setId("filters_data_help");
-	  filtersDataHelp.getElement().getFirstChildElement().getFirstChildElement().getFirstChildElement().setId("filters_panel");
-	  filtersDataHelp.getElement().getFirstChildElement().getFirstChildElement().getFirstChildElement().getNextSiblingElement().getNextSiblingElement().setId("help_panel");
-    filtersDataHelp.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_JUSTIFY);
-    return filtersDataHelp;
   }
 
   public void requestUpdatedData(FilterGroup filterGroup) {
@@ -271,6 +105,9 @@ public class AggregateUI implements EntryPoint {
   public void updateDataTable(SubmissionUISummary summary) {
 
     int headerIndex = 0;
+    dataTable.removeAllRows();
+    dataTable.getRowFormatter().addStyleName(0, "titleBar");
+    dataTable.addStyleName("dataTable");
     for(Column column : summary.getHeaders()) {
       dataTable.setText(0, headerIndex++, column.getDisplayHeader());
     }
@@ -289,29 +126,6 @@ public class AggregateUI implements EntryPoint {
    
   }
   
-  public VerticalPanel setupFormManagementPanel() {
-    Button uploadFormButton = new Button();
-    uploadFormButton.setHTML("<img src=\"images/blue_up_arrow.png\" /> Upload Form");
-    uploadTable.setWidget(0, 0, uploadFormButton);
-    
-    listOfForms.setText(0, 0, "Title");
-    listOfForms.setText(0, 1, "Form Id");
-    listOfForms.setText(0, 2, "User");
-    listOfForms.setText(0, 3, "Enabled");
-    listOfForms.setText(0, 4, "Publish");
-    listOfForms.setText(0, 5, "Export");
-    listOfForms.setText(0, 6, "Delete");
-    listOfForms.getRowFormatter().addStyleName(0, "titleBar");
-    listOfForms.addStyleName("dataTable");
-    
-    getFormList();
-
-    VerticalPanel formManagementPanel = new VerticalPanel();
-    formManagementPanel.add(uploadTable);
-    formManagementPanel.add(listOfForms);
-    return formManagementPanel;
-  }
-  
   /*
    * Creates a click handler for a main menu tab.
    * Defaults to the first sub-menu tab.
@@ -326,9 +140,9 @@ public class AggregateUI implements EntryPoint {
 			  hash.clear();
 			  hash.set(UrlHash.MAIN_MENU, s);
 			  TabPanel panel = null;
-			  if (s.equals(SUBMISSIONS))
+			  if (s.equals(SubmissionTabUI.SUBMISSIONS))
 				  panel = submissionNav;
-			  else if (s.equals(MANAGEMENT))
+			  else if (s.equals(ManageTabUI.MANAGEMENT))
 				  panel = manageNav;
 			  panel.selectTab(0);
 			  hash.put();
@@ -346,9 +160,10 @@ public class AggregateUI implements EntryPoint {
     view.add(def);
 	
 	// Create sub menu navigation
-	setupManageNav();
-	setupSubmissionNav();
-	
+    getFormList();
+    manageNav = new ManageTabUI(hash, listOfForms);
+    submissionNav = new SubmissionTabUI(hash, view, formsBox, filtersBox, 
+    		dataTable, def);
     mainNav.add(submissionNav, "Submissions");
     mainNav.add(manageNav, "Management");
     mainNav.addStyleName("mainNav");
@@ -370,75 +185,6 @@ public class AggregateUI implements EntryPoint {
     contentLoaded();
   }
   
-  private ClickHandler getSubMenuClickHandler(
-		  final String menu, final String subMenu) {
-	  return new ClickHandler() {
-		  @Override
-		  public void onClick(ClickEvent event) {
-			  hash.clear();
-			  hash.set(UrlHash.MAIN_MENU, menu);
-			  hash.set(UrlHash.SUB_MENU, subMenu);
-			  hash.put();
-		  }
-	  };
-  }
-  
-  public void setupManageNav() {
-	  manageNav.add(setupFormManagementPanel(), "Forms");
-	  manageNav.add(setupExportsPanel(), "Export");
-	  manageNav.add(setupPermissionsPanel(), "Permissions");
-	  manageNav.add(setupUtilitiesPanel(), "Utilities");
-	  
-	  int selected = 0;
-	  String subMenu = hash.get(UrlHash.SUB_MENU);
-	  for (int i = 0; i < MANAGEMENT_MENU.length; i++)
-		  if (subMenu.equals(MANAGEMENT_MENU[i]))
-			  selected = i;
-	  manageNav.selectTab(selected);
-
-	  for (int i = 0; i < MANAGEMENT_MENU.length; i++)
-		  manageNav.getTabBar().getTab(i).addClickHandler(getSubMenuClickHandler(MANAGEMENT, MANAGEMENT_MENU[i]));
-  }
-  
-  public FlexTable setupExportsPanel() {
-	  return new FlexTable();
-  }
-  
-  public HTML setupPermissionsPanel() {
-	  return new HTML("Content Forthcoming");
-  }
-  
-  public HTML setupUtilitiesPanel() {
-	  return new HTML("Content Forthcoming");
-  }
-  
-  public void setupSubmissionNav() {
-	  submissionNav.add(setupSubmissionsPanel(), "Filter");
-	  submissionNav.add(setupVisualizePanel(), "Visualize");
-	  
-	  int selected = 0;
-	  String subMenu = hash.get(UrlHash.SUB_MENU);
-	  for (int i = 0; i < SUBMISSION_MENU.length; i++)
-		  if (subMenu.equals(SUBMISSION_MENU[i]))
-			  selected = i;
-	  submissionNav.selectTab(selected);
-	  
-	  for (int i = 0; i < SUBMISSION_MENU.length; i++)
-		  submissionNav.getTabBar().getTab(i).addClickHandler(getSubMenuClickHandler(SUBMISSIONS, SUBMISSION_MENU[i]));
-  }
-  
-  public VerticalPanel setupSubmissionsPanel() {
-	  	VerticalPanel reportContent = new VerticalPanel();
-	    reportContent.add(setupFormsAndGoalsPanel());
-	    filterPanel = setupFiltersDataHelpPanel(view);
-	    reportContent.add(filterPanel);
-	    return reportContent;
-  }
-  
-  public HTML setupVisualizePanel() {
-	  return new HTML("Content Forthcoming");
-  }
-  
   // Let's JavaScript know that the GWT content has been loaded
   // Currently calls into javascript/resize.js, if we add more JavaScript
   // then that should be changed.
@@ -446,7 +192,7 @@ public class AggregateUI implements EntryPoint {
   	$wnd.gwtContentLoaded();
   }-*/;
 
-  private void getFormList() {
+  void getFormList() {
     // Initialize the service proxy.
     if (formSvc == null) {
       formSvc = GWT.create(FormService.class);
@@ -493,6 +239,7 @@ Set<String> existingForms = new HashSet<String>();
 				  if (hash.get(UrlHash.FORM).equals(form.getTitle()))
 					  formsBox.setItemSelected(formsBox.getItemCount() - 1, true);
 			  }
+			  formsBox.addItem("none");
 		  }
 	  }
 	  formsBox.addChangeHandler(new ChangeHandler() {
@@ -510,7 +257,7 @@ Set<String> existingForms = new HashSet<String>();
 	  filtersBox.clear();
 	  for(FilterGroup group : set.getGroups()) {
 		  filtersBox.addItem(group.getName());
-	  }
+	  };
   }
   
   private void getFilterList(final String id) {
---
+++ src/org/opendatakit/aggregate/client/SubmissionTabUI.java
@@ -0,0 +1,231 @@
+package org.opendatakit.aggregate.client;
+
+import java.util.List;
+
+import org.opendatakit.aggregate.client.filter.ColumnFilter;
+import org.opendatakit.aggregate.client.filter.ColumnFilterHeader;
+import org.opendatakit.aggregate.client.filter.CreateNewFilterPopup;
+import org.opendatakit.aggregate.client.filter.Filter;
+import org.opendatakit.aggregate.client.filter.FilterGroup;
+import org.opendatakit.aggregate.client.filter.RowFilter;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.TabPanel;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class SubmissionTabUI extends TabPanel {
+
+	// Submission Navigation
+	private static final String FILTER = "filter";
+	private static final String VISUALIZE = "visualize";
+	private static final String[] SUBMISSION_MENU = {FILTER, VISUALIZE};
+	static final String SUBMISSIONS = "submissions";
+	private HorizontalPanel filterPanel = new HorizontalPanel();
+	private FlexTable formAndGoalSelectionTable = new FlexTable();
+	private CreateNewFilterPopup filterPopup = new CreateNewFilterPopup();
+	private HorizontalPanel filtersDataHelp = new HorizontalPanel();
+	private UrlHash hash;
+	private List<FilterGroup> view;
+	private ListBox formsBox;
+	private ListBox filtersBox;
+	private FlexTable dataTable;
+	private FilterGroup def;
+	
+	public SubmissionTabUI(UrlHash hash, List<FilterGroup> view,
+			ListBox formsBox, ListBox filtersBox, FlexTable dataTable, 
+			FilterGroup def) {
+		super();
+		this.hash = hash;
+		this.view = view;
+		this.formsBox = formsBox;
+		this.filtersBox = filtersBox;
+		this.dataTable = dataTable;
+		this.def = def;
+		this.add(setupSubmissionsPanel(), "Filter");
+		this.add(setupVisualizePanel(), "Visualize");
+		
+		int selected = 0;
+		String subMenu = hash.get(UrlHash.SUB_MENU);
+		for (int i = 0; i < SUBMISSION_MENU.length; i++)
+			if (subMenu.equals(SUBMISSION_MENU[i]))
+				selected = i;
+		this.selectTab(selected);
+		  
+		for (int i = 0; i < SUBMISSION_MENU.length; i++)
+			this.getTabBar().getTab(i)
+				.addClickHandler(getSubMenuClickHandler(
+						SUBMISSIONS, SUBMISSION_MENU[i]));
+	}
+	
+	public VerticalPanel setupSubmissionsPanel() {
+		VerticalPanel reportContent = new VerticalPanel();
+		reportContent.add(setupFormsAndGoalsPanel());
+		filterPanel = setupFiltersDataHelpPanel(view);
+		reportContent.add(filterPanel);
+		return reportContent;
+	}
+	
+	public HTML setupVisualizePanel() {
+		return new HTML("Content Forthcoming");
+	}
+	
+	public HorizontalPanel setupFormsAndGoalsPanel() {
+		// list of forms
+		formAndGoalSelectionTable.setWidget(0, 0, formsBox);
+		// list of filters
+		formAndGoalSelectionTable.setWidget(0, 1, filtersBox);
+		// load form + filter
+		Button loadFormAndFilterButton = new Button("Load Filter");
+		formAndGoalSelectionTable.setWidget(0, 2, loadFormAndFilterButton);
+		formAndGoalSelectionTable.setHTML(0, 3, "&nbsp;&nbsp;");
+
+		// end goals vis, export, publish
+		Button exportButton = new Button("<img src=\"images/green_right_arrow.png\" /> Export");
+		formAndGoalSelectionTable.setWidget(0, 4, exportButton);
+		Button publishButton = new Button("<img src=\"images/green_right_arrow.png\" /> Publish");
+		formAndGoalSelectionTable.setWidget(0, 5, publishButton);
+
+		HorizontalPanel formsAndGoalsPanel = new HorizontalPanel();
+		formsAndGoalsPanel.add(formAndGoalSelectionTable);
+		formsAndGoalsPanel.getElement().setId("form_and_goals_panel");
+		return formsAndGoalsPanel;
+	}
+	
+	ClickHandler getSubMenuClickHandler(
+			final String menu, final String subMenu) {
+		return new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				hash.clear();
+				hash.set(UrlHash.MAIN_MENU, menu);
+				hash.set(UrlHash.SUB_MENU, subMenu);
+				hash.put();
+			}
+		};
+	}
+	
+	public HorizontalPanel setupFiltersDataHelpPanel(
+			List<FilterGroup> groups) {
+		//create filter tree
+		Tree activeFilters = new Tree();
+		TreeItem title = new TreeItem(new Label("Active Filters"));
+		activeFilters.addItem(title);
+		  
+		for (FilterGroup group : groups) {
+			TreeItem itemGroup = loadFilterGroup(group);
+			title.addItem(itemGroup);
+			title.setState(true);
+		}
+		
+		//add new filter button
+		Button newFilter = new Button();
+		newFilter.setHTML(
+				"<img src=\"images/yellow_plus.png\" /> New Filter");
+		newFilter.addClickHandler(new ClickHandler(){
+			@Override
+			public void onClick(ClickEvent event) {
+				filterPopup = new CreateNewFilterPopup(dataTable, def);
+				filterPopup.setPopupPositionAndShow(
+						new PopupPanel.PositionCallback() {
+							@Override
+							public void setPosition(int offsetWidth, int offsetHeight) {
+								int left = (Window.getClientWidth() - offsetWidth) / 2;
+								int top = (Window.getClientHeight() - offsetHeight) / 2;
+								filterPopup.setPopupPosition(left, top);
+							}
+						}
+					);
+			  filterPopup.addCloseHandler(new CloseHandler<PopupPanel>() {
+				  
+				  @Override
+				  public void onClose(CloseEvent<PopupPanel> event) {
+					  filterPanel.clear();
+					  setupFiltersDataHelpPanel(view);
+				  }
+				
+			  });
+			}
+		  });
+		  
+		  activeFilters.add(newFilter);
+		  filtersDataHelp.add(activeFilters);
+		  
+	    // view data
+	    dataTable.getRowFormatter().addStyleName(0, "titleBar");
+	    dataTable.addStyleName("dataTable");
+	    filtersDataHelp.add(dataTable);
+
+	    // view help
+	    VerticalPanel helpPanel = new VerticalPanel();
+	    for (int i = 1; i < 5; i++) {
+	      helpPanel.add(new HTML("Help Content " + i));
+	    }
+	    helpPanel.setStyleName("help_panel");
+	    filtersDataHelp.add(helpPanel);
+	    filtersDataHelp.getElement().setId("filters_data_help");
+		  filtersDataHelp.getElement().getFirstChildElement().getFirstChildElement().getFirstChildElement().setId("filters_panel");
+		  filtersDataHelp.getElement().getFirstChildElement().getFirstChildElement().getFirstChildElement().getNextSiblingElement().getNextSiblingElement().setId("help_panel");
+	    filtersDataHelp.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_JUSTIFY);
+	    return filtersDataHelp;
+	  }
+	  
+	  public TreeItem loadFilterGroup(final FilterGroup group) {
+		  TreeItem filterGroup = new TreeItem(
+				  new Label(group.getName()));
+		  
+		  final FlexTable filters = new FlexTable();
+		  
+		  int row = 0;
+		  for (Filter filter: group.getFilters()) {
+			  if(filter instanceof RowFilter) {
+				  RowFilter rowFilter = (RowFilter) filter;
+				  filters.setWidget(row, 0, new Label(
+						  rowFilter.getVisibility() + rowFilter.getColumn().getDisplayHeader() + 
+						  "where columns are " + rowFilter.getOperation() + 
+						  rowFilter.getInput()));
+			  } else if (filter instanceof ColumnFilter){
+				  ColumnFilter columnFilter = (ColumnFilter) filter;
+				  List<ColumnFilterHeader> columns = columnFilter.getColumnFilterHeaders();
+				  String columnNames = "";
+				  for(ColumnFilterHeader column: columns) {
+					  columnNames += " " + column.getColumn().getDisplayHeader();
+				  }
+				  filters.setWidget(row, 0, new Label(
+						  columnFilter.getVisibility() + columnNames));
+			  }
+			  final Button removeFilter = new Button("-");
+			  filters.setWidget(row, 1, removeFilter);
+			  removeFilter.getElement().setPropertyObject(
+					  "filter", filter);
+			  removeFilter.addClickHandler(new ClickHandler() {
+
+				@Override
+				public void onClick(ClickEvent event) {
+					Filter remove = (Filter)removeFilter.getElement()
+						.getPropertyObject("filter");
+					group.removeFilter(remove);
+					filterPanel.clear();
+					setupFiltersDataHelpPanel(view);
+				} 
+			  });
+			  row++;
+		  }
+		  filterGroup.addItem(filters);
+		  filterGroup.setState(true);
+		  return filterGroup;
+	  }
+}
