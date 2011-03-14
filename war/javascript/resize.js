41b00dc3c73256973e92c68e2af125aa86c2e594
ngarrett 21 <ngarrett.21@gmail.com>
2011-03-13 21:48:04 -0700

    Pulled Help Panel out of navigation

--- war/javascript/resize.js
+++ war/javascript/resize.js
@@ -4,11 +4,11 @@ var MAX_SIDE_PANEL_WIDTH = 200;
 // the screen size.
 function onWindowResize() {
 	var width = $(window).width();
-	$(".mainNav").width(width);
-	
 	var sidePanelPotentialWidth = width * .15;
 	var sidePanelActualWidth = Math.min(MAX_SIDE_PANEL_WIDTH, sidePanelPotentialWidth);
+	$(".mainNav").width(width - sidePanelActualWidth);
+	
 	$("#filters_panel").width(sidePanelActualWidth);
 	$("#help_panel").width(sidePanelActualWidth);
-	$("#filters_data_help").width(width);
+	$("#filters_data").width(width - sidePanelActualWidth);
 }
