9269ddcf54cfaed33d48c00c51ad4320d0b537fa
ngarrett 21 <ngarrett.21@gmail.com>
2011-03-14 21:05:10 -0700

    Fixed scrolling and help menu layout

--- war/javascript/resize.js
+++ war/javascript/resize.js
@@ -4,12 +4,20 @@ var MAX_SIDE_PANEL_WIDTH = 200;
 // the screen size.
 function onWindowResize() {
 	var width = $(window).width();
-	var sidePanelPotentialWidth = width * .15;
+	var height = $(window).height();
+	
+	var sidePanelPotentialWidth = width * .2;
 	var sidePanelActualWidth = Math.min(MAX_SIDE_PANEL_WIDTH, sidePanelPotentialWidth);
+	
 	$(".mainNav").width(width - sidePanelActualWidth);
-
-	$("#help_panel").width(sidePanelActualWidth);
-	$("#filters_panel").width(sidePanelActualWidth);
+	$("#help_container").width(sidePanelActualWidth);
+	$("#filters_container").width(sidePanelActualWidth);
 	$("#submission_container").width(width - (2 * sidePanelActualWidth));
 	$("#form_management_table").width(width - sidePanelActualWidth);
+	
+	$("#dynamic_content").height(height - 24);
+	$("#layout_panel").height(height - $("#layout_panel").offset().top - 24);
+	$("#filters_container").height(height - $("#filters_container").offset().top);
+	$("#submission_container").height(height - $("#submission_container").offset().top);
+	$("#help_container").height(height - $("#help_container").offset().top);
 }
