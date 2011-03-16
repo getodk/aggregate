b7f5351159d7ad158b4045ba7192b2e53037d81c
wbrunette <devnull@localhost>
2011-03-16 01:01:29 -0700

    Updates for configuring to run on App Engine

--- src/org/opendatakit/aggregate/constants/common/UIConsts.java
+++ src/org/opendatakit/aggregate/constants/common/UIConsts.java
@@ -3,4 +3,13 @@ package org.opendatakit.aggregate.constants.common;
 public class UIConsts {
   public static final String URI_DEFAULT = "no uuid";
   public static final String FSC_URI_PARAM = "fsc";
+
+  // running locally
+  public static final String GOOGLE_MAP_KEY = "";
+  public static final String CURRENT_HOST_ADDR = "localhost:8888/";
+
+  
+  // google app engine deployment
+//  public static final String GOOGLE_MAP_KEY = "ABQIAAAABkAadQ7rAra4OaknnxN0MhSHx-tc1xWV6Pfl7CPQIU_6shZXHxTQ4sjqmZn7zqPwIfQP9eG5A9VVKg";
+//  public static final String CURRENT_HOST_ADDR = "odk-wb-2.appspot.com/";
 }
