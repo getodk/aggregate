ef9c7ecbd009bb4863b4b0c5216a87ef8fe38309
wbrunette <devnull@localhost>
2011-04-26 17:25:08 -0700

    Bug fixes and remove url constant

--- src/org/opendatakit/aggregate/constants/common/UIConsts.java
+++ src/org/opendatakit/aggregate/constants/common/UIConsts.java
@@ -6,10 +6,7 @@ public class UIConsts {
 
   // running locally
   public static final String GOOGLE_MAP_KEY = "";
-  public static final String CURRENT_HOST_ADDR = "localhost:8888/";
-
   
   // google app engine deployment
 //  public static final String GOOGLE_MAP_KEY = "ABQIAAAABkAadQ7rAra4OaknnxN0MhSHx-tc1xWV6Pfl7CPQIU_6shZXHxTQ4sjqmZn7zqPwIfQP9eG5A9VVKg";
-//  public static final String CURRENT_HOST_ADDR = "odk-wb-2.appspot.com/";
 }
