df1d21962ba5f9081556185b62b251e943e3db24
msundt <msundt@cs.washington.edu>
2011-05-05 16:52:32 -0700

    Remove conf, war and test directories.  Now under src/main/resources, eclipse-aggregate-gae, and src/test

--- war/javascript/main.js
+++
@@ -1,7 +0,0 @@
-// Runs after gwt is finished putting elements on the page
-function gwtContentLoaded() {
-	// Set up window resize handler and call it.
-	//$("#help_container").offset({left: $("#help_container").offset().left, top: $("#second_level_menu").offset().top});
-	$(window).resize(onWindowResize);
-	onWindowResize();
-}
