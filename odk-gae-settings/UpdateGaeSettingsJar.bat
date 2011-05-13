74d5b4ea19acd4b0b4dc294fa80f29700a10f4b5
msundt <msundt@cs.washington.edu>
2011-05-12 17:33:06 -0700

    Remove odk-gae-settings.jar Fix errors in Windows batch script.

--- odk-gae-settings/UpdateGaeSettingsJar.bat
+++ odk-gae-settings/UpdateGaeSettingsJar.bat
@@ -1,5 +1,6 @@
+if NOT EXIST target mkdir target
 del target\odk-gae-settings-latest.jar
 jar -cf target\odk-gae-settings-latest.jar -C ..\src\main\resources\gae . -C ..\src\main\resources\common .
-copy /Y target\odk-gae\settings-latest.jar ..\eclipse-aggregate-gae\war\WEB-INF\lib
+copy /Y target\odk-gae-settings-latest.jar ..\eclipse-aggregate-gae\war\WEB-INF\lib
 
 
