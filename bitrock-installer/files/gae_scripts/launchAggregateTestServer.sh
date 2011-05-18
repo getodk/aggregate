c9f9e87efc4a69f58ad6a43210ce8bfc22f68a45
msundt <msundt@cs.washington.edu>
2011-05-18 13:42:05 -0700

    Update scripts for GAE deployment and add development server execution instructions.

--- bitrock-installer/files/gae_scripts/launchAggregateTestServer.sh
+++ bitrock-installer/files/gae_scripts/launchAggregateTestServer.sh
@@ -2,4 +2,6 @@
 # Launches the development AppServer
 [ -z "${DEBUG}" ] || set -x  # trace if $DEBUG env. var. is non-zero
 UPLOAD_ROOT=`dirname $0 | sed -e "s#^\\([^/]\\)#${PWD}/\\1#"` # sed makes absolute
-@$UPLOAD_ROOT/bin/dev_appserver.sh ODKAggregate
+$UPLOAD_ROOT/appengine-java-sdk/bin/dev_appserver.sh ODKAggregate
+echo Press any key to continue . . .
+read
