1666b57c3200408a2bf66627f910c26a7d27b36f
msundt <msundt@cs.washington.edu>
2011-05-20 16:23:27 -0700

    Replace script files with an Ant build script. Updates to CONFIGURE.txt

--- odk-gae-settings/UpdateGaeSettingsJar.bat
+++
@@ -1,6 +0,0 @@
-if NOT EXIST target mkdir target
-del target\odk-gae-settings-latest.jar
-jar -cf target\odk-gae-settings-latest.jar -C ..\src\main\resources\gae . -C ..\src\main\resources\common .
-copy /Y target\odk-gae-settings-latest.jar ..\eclipse-aggregate-gae\war\WEB-INF\lib
-
-
