881e6cf897e8402284205672a56d5160f7659b62
msundt <msundt@cs.washington.edu>
2011-05-31 16:49:51 -0700

    Updated install script with customized installer strings.

--- bitrock-installer/files/gae_scripts/uploadAggregateToAppEngine.sh
+++ bitrock-installer/files/gae_scripts/uploadAggregateToAppEngine.sh
@@ -2,6 +2,6 @@
 # Launches AppCfg
 [ -z "${DEBUG}" ] || set -x  # trace if $DEBUG env. var. is non-zero
 UPLOAD_ROOT=`dirname $0 | sed -e "s#^\\([^/]\\)#${PWD}/\\1#"` # sed makes absolute
-$UPLOAD_ROOT/appengine-sdk-java/bin/appcfg.sh --enable_jar_splitting update ODKAggregate
+$UPLOAD_ROOT/appengine-sdk-java/bin/appcfg.sh --passin update ODKAggregate
 echo Press any key to continue . . .
 read
