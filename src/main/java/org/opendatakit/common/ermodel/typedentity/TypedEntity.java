0ccc6ccf91a8f41b16edb8bf0db86172692dfb2a
Dylan Price <the.dylan.price@gmail.com>
2011-06-16 23:53:43 -0700

    Finished adding docs to typedentity classes.

--- src/main/java/org/opendatakit/common/ermodel/typedentity/TypedEntity.java
+++ src/main/java/org/opendatakit/common/ermodel/typedentity/TypedEntity.java
@@ -6,62 +6,110 @@ import org.opendatakit.common.persistence.exception.ODKDatastoreException;
 import org.opendatakit.common.persistence.exception.ODKEntityPersistException;
 import org.opendatakit.common.web.CallingContext;
 
-
 /**
- * A RelationEntity defines an entity for a specific relation.
+ * <p>
+ * A TypedEntity is an entity which belongs to a specific relation.
+ * </p>
+ * 
+ * <p>
+ * TypedEntity is mutable and not threadsafe.
+ * </p>
  * 
  * @author the.dylan.price@gmail.com
- *
+ * 
  */
 public abstract class TypedEntity
 {
+    /**
+     * The relation this TypedEntity belongs to.
+     */
     private TypedEntityRelation<?> relation;
+
+    /**
+     * The generic entity backing this TypedEntity.
+     */
     private Entity entity;
-    
+
+    /**
+     * Creates a new, empty TypedEntity from the given relation.
+     * 
+     * @param relation
+     *            the relation to create an entity for.
+     */
     protected TypedEntity(TypedEntityRelation<?> relation)
     {
         this.relation = relation;
         this.entity = relation.newEntity(getCC());
     }
-    
+
+    /**
+     * Creates a TypedEntity which is backed by the given existing entity.
+     * 
+     * @param relation
+     *            the relation the given entity belongs to.
+     * @param entity
+     *            the generic entity. This entity must be an entity of the given
+     *            relation.
+     */
     protected TypedEntity(TypedEntityRelation<?> relation, Entity entity)
     {
-       this.relation = relation;
-       this.entity = entity;
+        this.relation = relation;
+        this.entity = entity;
     }
-    
+
+    /**
+     * @return the backing generic entity of this TypedEntity.
+     */
     protected Entity getEntity()
     {
         return this.entity;
     }
-    
+
+    /**
+     * @return the CallingContext saved by the relation this TypedEntity belongs to.
+     */
     protected CallingContext getCC()
     {
         return relation.getCC();
     }
-    
+
+    /**
+     * {@link org.opendatakit.common.ermodel.Relation#getDataField(String)}
+     */
     protected DataField getDataField(String fieldName)
     {
         return relation.getDataField(fieldName);
     }
-    
+
+    /**
+     * {@link org.opendatakit.common.ermodel.Entity#getUri()}
+     */
     public String getUri()
     {
         return this.entity.getUri();
     }
-    
+
+    /**
+     * Saves this TypedEntity to the datastore.
+     * 
+     * @throws ODKEntityPersistException
+     */
     public void save() throws ODKEntityPersistException
     {
         this.entity.persist(getCC());
     }
-    
+
+    /**
+     * Permanently deletes this TypedEntity from the datastore.
+     * 
+     * @throws ODKDatastoreException
+     */
     public void delete() throws ODKDatastoreException
     {
         try
         {
             this.entity.remove(getCC());
-        }
-        catch (ODKDatastoreException e)
+        } catch (ODKDatastoreException e)
         {
             // try one more time
             this.entity.remove(getCC());
--- src/main/java/org/opendatakit/common/ermodel/typedentity/TypedEntityQuery.java
+++ src/main/java/org/opendatakit/common/ermodel/typedentity/TypedEntityQuery.java
@@ -13,7 +13,16 @@ import org.opendatakit.common.persistence.Query.FilterOperation;
 import org.opendatakit.common.persistence.exception.ODKDatastoreException;
 
 /**
- * A RelationQuery defines a query over a specific Relation. TODO: document
+ * <p>
+ * Query capabilities for {@link TypedEntityRelation}. Allows the results of
+ * queries to be TypedEntity objects instead of generic Entity objects. You
+ * usually obtain a TypedEntityQuery through a {@link TypedEntityRelation}'s
+ * {@link TypedEntityRelation#query() query()} method.
+ * </p>
+ * 
+ * <p>
+ * TypedEntityQuery is mutable and not threadsafe.
+ * </p>
  * 
  * @author the.dylan.price@gmail.com
  * 
@@ -25,40 +34,114 @@ public class TypedEntityQuery<T extends TypedEntity>
      * The relation to query.
      */
     private TypedEntityRelation<T> relation;
+    /**
+     * The underlying datastore query.
+     */
     private Query query;
 
+    /**
+     * Create a new TypedEntityQuery. Typically you should create a
+     * TypedEntityQuery through a {@link TypedEntityRelation}'s
+     * {@link TypedEntityRelation#query() query()} method.
+     * 
+     * @param relation
+     *            an instance of the relation to query.
+     */
     public TypedEntityQuery(TypedEntityRelation<T> relation)
     {
         this.relation = relation;
         this.query = relation.createQuery();
     }
 
+    /**
+     * Add an equal filter to the query.
+     * 
+     * @param fieldName
+     *            the name of a field in the relation.
+     * @param value
+     *            the value the given field should be equal to. This must be of
+     *            the correct type for the corresponding field.
+     * @return this TypedEntityQuery, with the equal filter added.
+     */
     public TypedEntityQuery<T> equal(String fieldName, Object value)
     {
         return addFilter(fieldName, FilterOperation.EQUAL, value);
     }
 
+    /**
+     * Add a greater than filter to the query.
+     * 
+     * @param fieldName
+     *            the name of a field in the relation.
+     * @param value
+     *            the value the given field should greater than. This must be of
+     *            the correct type for the corresponding field.
+     * @return this TypedEntityQuery, with the greater than filter added.
+     */
     public TypedEntityQuery<T> greaterThan(String fieldName, Object value)
     {
         return addFilter(fieldName, FilterOperation.GREATER_THAN, value);
     }
 
+    /**
+     * Add a greater than or equal filter to the query.
+     * 
+     * @param fieldName
+     *            the name of a field in the relation.
+     * @param value
+     *            the value the given field should greater than or equal to.
+     *            This must be of the correct type for the corresponding field.
+     * @return this TypedEntityQuery, with the greater than or equal filter
+     *         added.
+     */
     public TypedEntityQuery<T> greaterThanOrEqual(String fieldName, Object value)
     {
         return addFilter(fieldName, FilterOperation.GREATER_THAN_OR_EQUAL,
                 value);
     }
 
+    /**
+     * Add a less than filter to the query.
+     * 
+     * @param fieldName
+     *            the name of a field in the relation.
+     * @param value
+     *            the value the given field should less than. This must be of
+     *            the correct type for the corresponding field.
+     * @return this TypedEntityQuery, with the less than filter added.
+     */
     public TypedEntityQuery<T> lessThan(String fieldName, Object value)
     {
         return addFilter(fieldName, FilterOperation.LESS_THAN, value);
     }
 
+    /**
+     * Add a less than or equal filter to the query.
+     * 
+     * @param fieldName
+     *            the name of a field in the relation.
+     * @param value
+     *            the value the given field should less than or equal to. This
+     *            must be of the correct type for the corresponding field.
+     * @return this TypedEntityQuery, with the less than or equal filter added.
+     */
     public TypedEntityQuery<T> lessThanOrEqual(String fieldName, Object value)
     {
         return addFilter(fieldName, FilterOperation.LESS_THAN_OR_EQUAL, value);
     }
 
+    /**
+     * Adds a filter to the query.
+     * 
+     * @param fieldName
+     *            the name of a field in the relation.
+     * @param op
+     *            the operation to filter with.
+     * @param value
+     *            the value to filter with. This must be of the correct type for
+     *            the corresponding field.
+     * @return this TypedEntityQuery, with the given filter added.
+     */
     private TypedEntityQuery<T> addFilter(String fieldName, FilterOperation op,
             Object value)
     {
@@ -67,16 +150,42 @@ public class TypedEntityQuery<T extends TypedEntity>
         return this;
     }
 
+    /**
+     * Adds an ascending sort to the query.
+     * 
+     * @param fieldName
+     *            the name of the field to sort by. This field must be a field
+     *            in the relation of this query.
+     * @return this TypedEntityQuery, with the ascending sort added.
+     */
     public TypedEntityQuery<T> sortAscending(String fieldName)
     {
         return addSort(fieldName, Direction.ASCENDING);
     }
 
+    /**
+     * Adds an descending sort to the query.
+     * 
+     * @param fieldName
+     *            the name of the field to sort by. This field must be a field
+     *            in the relation of this query.
+     * @return this TypedEntityQuery, with the descending sort added.
+     */
     public TypedEntityQuery<T> sortDescending(String fieldName)
     {
         return addSort(fieldName, Direction.DESCENDING);
     }
 
+    /**
+     * Adds a sort to the query.
+     * 
+     * @param fieldName
+     *            the name of the field to sort by. This field must be a field
+     *            in the relation of this query.
+     * @param direction
+     *            the direction to sort by.
+     * @return this TypedEntityQuery, with the given sort added.
+     */
     private TypedEntityQuery<T> addSort(String fieldName, Direction direction)
     {
         DataField field = relation.getDataField(fieldName);
@@ -84,6 +193,20 @@ public class TypedEntityQuery<T extends TypedEntity>
         return this;
     }
 
+    /**
+     * Narrows the scope of the query to only include entities whose value for
+     * the given fieldName is in values.
+     * 
+     * @param fieldName
+     *            the name of the field to filter with. This must be a field in
+     *            the relation of this query.
+     * @param values
+     *            the values to filter by. This collection must be of the
+     *            correct type for the field identified by fieldName.
+     * @return this TypedEntityQuery, with the include filter added. All
+     *         entities with values not in values will be excluded from the
+     *         query.
+     */
     public TypedEntityQuery<T> include(String fieldName, Collection<?> values)
     {
         DataField field = relation.getDataField(fieldName);
@@ -91,6 +214,14 @@ public class TypedEntityQuery<T extends TypedEntity>
         return this;
     }
 
+    /**
+     * Get the single entity result of the query.
+     * 
+     * @return the entity which is the sole result of executing this query.
+     * @throws ODKDatastoreException
+     *             if the query contains no entities or greater than one entity,
+     *             or if there is a problem communicating with the datastore.
+     */
     public T get() throws ODKDatastoreException
     {
         List<T> results = execute(1);
@@ -107,11 +238,27 @@ public class TypedEntityQuery<T extends TypedEntity>
         return results.get(0);
     }
 
+    /**
+     * Execute the query and return a list of all results. Equivalent to calling
+     * {@link #execute(int) execute(0)}.
+     * 
+     * @return a list of all the entities which matched the query.
+     * @throws ODKDatastoreException
+     */
     public List<T> execute() throws ODKDatastoreException
     {
         return execute(0);
     }
 
+    /**
+     * Execute the query and return a list of results.
+     * 
+     * @param limit
+     *            the maximum number of entities to retrieve, or 0 for no limit.
+     * @return a list of the entities which matched the query. The size of this
+     *         list will be no greater than limit.
+     * @throws ODKDatastoreException
+     */
     public List<T> execute(int limit) throws ODKDatastoreException
     {
         List<Entity> genericEntities = relation.executeQuery(this.query, limit);
@@ -124,6 +271,15 @@ public class TypedEntityQuery<T extends TypedEntity>
         return relationEntities;
     }
 
+    /**
+     * Retrieves all distinct values for the given field, with any sort and
+     * filter criteria.
+     * 
+     * @param fieldName
+     *            the name of the field to retrieve distinct values for.
+     * @return a list of distinct values for the given field, narrowed by any
+     *         existing filter and sort criteria.
+     */
     public List<?> getDistinct(String fieldName)
     {
         DataField dataField = relation.getDataField(fieldName);
