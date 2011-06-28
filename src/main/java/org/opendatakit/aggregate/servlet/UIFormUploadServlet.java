bec6145bcb7185b46419cb34dbaf75e232a15916
msundt <msundt@cs.washington.edu>
2011-06-28 12:25:11 -0700

    Move upload_control.js from /res to /javascript. Add more exceptions for images, resources and stylesheets to channel security filter.

--- src/main/java/org/opendatakit/aggregate/servlet/UIFormUploadServlet.java
+++ src/main/java/org/opendatakit/aggregate/servlet/UIFormUploadServlet.java
@@ -73,7 +73,7 @@ public class UIFormUploadServlet extends ServletUtilBase {
    */
   private static final String JQUERY_SCRIPT_HEADER = "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js\"></script>";
 
-  private static final String UPLOAD_SCRIPT_RESOURCE = "res/upload_control.js";
+  private static final String UPLOAD_SCRIPT_RESOURCE = "javascript/upload_control.js";
   
   private static final String PAGE_STYLE_RESOURCE = "stylesheets/upload.css";
   
