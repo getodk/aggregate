a2a310a1f94a7683f2547a1f4138327c3c069740
msundt <msundt@cs.washington.edu>
2011-06-29 11:40:05 -0700

    Tentative fix for issue 245 - browser stuck in refresh.

--- src/main/java/org/opendatakit/aggregate/client/RefreshTimer.java
+++ src/main/java/org/opendatakit/aggregate/client/RefreshTimer.java
@@ -18,88 +18,143 @@ package org.opendatakit.aggregate.client;
 
 import org.opendatakit.aggregate.constants.common.SubTabs;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Timer;
 
+/**
+ * Background refresh timer that polls the server every 5 seconds
+ * for changes to whichever tab is currently selected.  If no
+ * UI interaction has happened for 5 minutes, the refreshes stop. 
+ * 
+ * @author wbrunette@gmail.com
+ * @author mitchellsundt@gmail.com
+ *
+ */
 public class RefreshTimer extends Timer {
- // private static final int REFRESH_INTERVAL = 100000; // ms
 
+  // REFRESH_INTERVAL is the interval between callbacks.
+  // Upon testing, this is the scheduling interval, not the 
+  // inter-callback interval.  So if the computer on which the 
+  // browser is running is slow, you can get a backlog of 
+  // callbacks queued up.
   private static final int REFRESH_INTERVAL = 5000; // ms
-  private static final int STALL_INTERVALS = 60; // 5 min / 5 sec
+  // private static final int REFRESH_INTERVAL = 100000; // ms
+  
+  // STALL_INTERVALS is the number of intervals of no UI
+  // interaction after which the timer will be stopped.
+  private static final int UI_STALL_INTERVALS = 60; // 5 min / 5 sec
+  
+  // lastCompletionTime tracks the completion timestamp 
+  // of the last timer action.  Used to detect and skip
+  // any timer callback backlog.
+  private long lastCompletionTime = 0L;
+  
+  // intervalsElapsed counts the intervals since a UI interaction
   private int intervalsElapsed = 0;
+  
+  // isActive tracks the active/cancelled state of the timer
+  // the GWT timer doesn't provide this information.
+  private boolean isActive = false;
+  
   private AggregateUI aggregateUI;
 
   private SubTabs currentSubTab;
 
   public RefreshTimer(AggregateUI ui) {
     aggregateUI = ui;
-
-    // Setup timer to refresh list automatically.
-    scheduleRepeating(REFRESH_INTERVAL);
+    restartTimer();
   }
 
   public void setCurrentSubTab(SubTabs subtab) {
     currentSubTab = subtab;
-    restartTimer();
+    refreshNow();
   }
 
   public void refreshNow() {
-    // cause an update
-    run();
+    // reset the ui inactivity counter.
     restartTimer();
+    // set the lastCompletionTime to zero
+    // this bypasses the backlog check.
+    lastCompletionTime = 0L;
+    // trigger a run of the timer.
+    run();
+  }
+  
+  @Override
+  public void cancel() {
+	  super.cancel();
+	  isActive = false;
   }
   
   public void restartTimer() {
-    // stop the auto refresh
-    cancel();
-
-    // Restart timer to refresh list automatically.
-    scheduleRepeating(REFRESH_INTERVAL);
-    intervalsElapsed = 0;
+	if ( isActive ) {
+		// just reset the intervalsElapsed
+		intervalsElapsed = 0;
+	} else {
+		// reset the ui inactivity counter
+		// restart the periodic timer
+		// set the isActive flag
+		intervalsElapsed = 0;
+		scheduleRepeating(REFRESH_INTERVAL);
+		isActive = true;
+	}
   }
 
   @Override
   public void run() {
-    if (intervalsElapsed == STALL_INTERVALS) {
-      this.cancel();
+	long timeRefreshStart = System.currentTimeMillis();
+	if ( lastCompletionTime + REFRESH_INTERVAL > timeRefreshStart ) {
+		// timer is backed up -- flush the queued callbacks
+		GWT.log("timer is backed up -- skipping");
+		return;
+	}
+	
+    if (intervalsElapsed == UI_STALL_INTERVALS) {
+    	// this appears to be an idle UI - stop all refresh polling.
+    	cancel();
     }
-    intervalsElapsed++;
+    intervalsElapsed++;	
     
-    if(currentSubTab == null) {
-      return;
-    }
     SubTabInterface tabPanel;
     
-    switch (currentSubTab) {
-    case FORMS:
-      if ((intervalsElapsed % 3) == 0) {
-        tabPanel = aggregateUI.getManageNav().getSubTab(currentSubTab);
-        tabPanel.update();
-      }
-      break;
-    case FILTER:
-      if ((intervalsElapsed % 3) == 0) {
-        tabPanel = aggregateUI.getSubmissionNav().getSubTab(currentSubTab);
-        tabPanel.update();
-      }
-      break;
-    case PUBLISH:
-      tabPanel = aggregateUI.getManageNav().getSubTab(currentSubTab);
-      tabPanel.update();
-      break;
-    case EXPORT:
-      tabPanel = aggregateUI.getSubmissionNav().getSubTab(currentSubTab);
-      tabPanel.update();
-      break;
-    case PREFERENCES:
-      if ((intervalsElapsed % 6) == 0) {
-        tabPanel = aggregateUI.getManageNav().getSubTab(currentSubTab);
-        tabPanel.update();
-      }
-      break;
-    default:
-      // should not happen
-
+    if(currentSubTab != null) {
+	    switch (currentSubTab) {
+	    case FORMS:
+	      if ((intervalsElapsed % 3) == 0) {
+	        tabPanel = aggregateUI.getManageNav().getSubTab(currentSubTab);
+	        tabPanel.update();
+	      }
+	      break;
+	    case FILTER:
+	      if ((intervalsElapsed % 3) == 0) {
+	        tabPanel = aggregateUI.getSubmissionNav().getSubTab(currentSubTab);
+	        tabPanel.update();
+	      }
+	      break;
+	    case PUBLISH:
+	      tabPanel = aggregateUI.getManageNav().getSubTab(currentSubTab);
+	      tabPanel.update();
+	      break;
+	    case EXPORT:
+	      tabPanel = aggregateUI.getSubmissionNav().getSubTab(currentSubTab);
+	      tabPanel.update();
+	      break;
+	    case PREFERENCES:
+	      if ((intervalsElapsed % 6) == 0) {
+	        tabPanel = aggregateUI.getManageNav().getSubTab(currentSubTab);
+	        tabPanel.update();
+	      }
+	      break;
+	    default:
+	      // should not happen
+	      GWT.log("currentSubTab has no defined action in RefreshTimer.run()");
+	    }
     }
-
+    // record last completion time...
+    lastCompletionTime = System.currentTimeMillis();
+    long timerActionDuration = lastCompletionTime - timeRefreshStart;
+    if ( timerActionDuration > REFRESH_INTERVAL ) {
+    	GWT.log("update time " + Long.toString(timerActionDuration));
+    }
   }
 }
