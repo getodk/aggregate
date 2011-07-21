2f8d1a44d27e857388b5a83e7a6908405c841dd2
Dylan Price <the.dylan.price@gmail.com>
2011-07-21 00:35:07 -0700

    Updated simple relation layer. All working now.

--- src/main/java/org/opendatakit/common/ermodel/simple/Attribute.java
+++ src/main/java/org/opendatakit/common/ermodel/simple/Attribute.java
@@ -1,5 +1,9 @@
 package org.opendatakit.common.ermodel.simple;
 
+import org.opendatakit.common.persistence.DataField;
+import org.opendatakit.common.persistence.DataField.DataType;
+import org.opendatakit.common.utils.Check;
+
 public class Attribute
 {
     private final String name;
@@ -9,7 +13,7 @@ public class Attribute
     public Attribute(String name, AttributeType type, boolean nullable)
     {
         Check.notNullOrEmpty(name, "name");
-        Check.notNull(type);
+        Check.notNull(type, "type");
 
         this.name = name;
         this.type = type;
@@ -33,7 +37,57 @@ public class Attribute
 
     protected DataField toDataField()
     {
-        return new DataField(this.name, DataType.valueOf(this.type.name()), this.nullable);
+        return new DataField(this.name, DataType.valueOf(this.type.name()),
+                this.nullable);
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString()
+    {
+        return String.format("Attribute [name=%s, type=%s, nullable=%s]", name,
+                type, nullable);
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + (nullable ? 1231 : 1237);
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Attribute))
+            return false;
+        Attribute other = (Attribute) obj;
+        if (name == null)
+        {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (nullable != other.nullable)
+            return false;
+        if (type != other.type)
+            return false;
+        return true;
     }
 }
-
--- src/main/java/org/opendatakit/common/ermodel/simple/Relation.java
+++ src/main/java/org/opendatakit/common/ermodel/simple/Relation.java
@@ -1,13 +1,13 @@
 package org.opendatakit.common.ermodel.simple;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.opendatakit.common.ermodel.Entity;
 import org.opendatakit.common.ermodel.ExtendedAbstractRelation;
-import org.opendatakit.common.ermodel.TableNamespace;
 import org.opendatakit.common.persistence.DataField;
-import org.opendatakit.common.persistence.Query;
 import org.opendatakit.common.persistence.exception.ODKDatastoreException;
+import org.opendatakit.common.persistence.exception.ODKEntityNotFoundException;
+import org.opendatakit.common.utils.Check;
 import org.opendatakit.common.web.CallingContext;
 
 /**
@@ -16,11 +16,12 @@ import org.opendatakit.common.web.CallingContext;
 public class Relation
 {
 
-    private ExtendedAbstractRelation relation;
+    private final ExtendedAbstractRelation relation;
+    private final String namespace;
+    private final String name;
 
-    public Relation(String namespace, String name,
-            List<Attribute> attributes, CallingContext cc)
-            throws ODKDatastoreException
+    public Relation(String namespace, String name, List<Attribute> attributes,
+            CallingContext cc) throws ODKDatastoreException
     {
         Check.notNullOrEmpty(namespace, "namespace");
         Check.notNullOrEmpty(name, "name");
@@ -28,50 +29,134 @@ public class Relation
         Check.notNull(cc, "cc");
 
         // Add attributes to AttributeRelation
-        AttributeRelation attributeRelation = AttributeRelation.getInstance(namespace, cc);
+        // Of course, if we are constructing the AttributeRelation itself then we skip this
+        if (!name.equals(AttributeRelation.name()))
+        {
+            for (Attribute attribute : attributes)
+            {
+                AttributeRelation attributeRelation = AttributeRelation
+                        .getInstance(namespace, cc);
+                Entity attrEntity = attributeRelation.newEntity();
+
+                attrEntity.setString(AttributeRelation.RELATION_NAMESPACE,
+                        namespace);
+                attrEntity.setString(AttributeRelation.RELATION_NAME, name);
+                attrEntity.setString(AttributeRelation.NAME,
+                        attribute.getName());
+                attrEntity.setString(AttributeRelation.TYPE, attribute
+                        .getType().name());
+                attrEntity.setBoolean(AttributeRelation.NULLABLE,
+                        attribute.isNullable());
+
+                attrEntity.save();
+            }
+        }
+
+        // Convert to DataFields
         List<DataField> fields = new ArrayList<DataField>();
         for (Attribute attribute : attributes)
         {
             fields.add(attribute.toDataField());
-            Entity attribute = attributeRelation.newEntity();
-            attribute.setString(AttributeRelation.NAME, attribute.getName());
-            attribute.setString(AttributeRelation.TYPE, attribute.getType().name());
-            attribute.setString(AttributeRelation.NULLABLE, attribute.isNullable());
-            attribute.save();
         }
 
         // Create relation
-        this.relation = new ExtendedAbstractRelation(namespace, name,
-                fields, cc);
+        this.relation = new ExtendedAbstractRelation(namespace, name, fields,
+                cc);
+        this.namespace = namespace;
+        this.name = name;
     }
 
     private Relation(String namespace, String name, CallingContext cc)
+            throws ODKDatastoreException
     {
         Check.notNullOrEmpty(namespace, "namespace");
         Check.notNullOrEmpty(name, "name");
         Check.notNull(cc, "cc");
 
         // Get attributes from AttributeRelation
-        AttributeRelation attributeRelation = AttributeRelation.getInstance(namespace, cc);
-        List<Entity> attributes = attributeRelation.query().equal(AttributeRelation.RELATION_NAMESPACE, namespace).equal(AttributeRelation.RELATION_NAME, name).execute();
-        List<DataField> fields = new ArrayList<DataField>();
-        for (Entity attribute : attributes)
+        List<Attribute> attributes = getAttributes(namespace, name, cc);
+        if (attributes.isEmpty())
         {
-            String attributeName = attribute.getString(AttributeRelation.NAME);
-            String attributeType = DataType.valueOf(attribute.getString(AttributeRelation.TYPE));
-            boolean attributeNullable = attribute.getBoolean(AttributeRelation.NULLABLE);
-            DataField field = new DataField(attributeName, attributeType, attributeNullable);
-            fields.add(field);
+            throw new ODKDatastoreException(String.format(
+                    "Relation '%s' in namespace '%s' does not exist.", name,
+                    namespace));
+        }
+        List<DataField> fields = new ArrayList<DataField>();
+        for (Attribute attribute : attributes)
+        {
+            fields.add(attribute.toDataField());
         }
 
         // Get relation
-        this.relation = new ExtendedAbstractRelation(namespace, name, fields, cc);
+        this.relation = new ExtendedAbstractRelation(namespace, name, fields,
+                cc);
+        this.namespace = namespace;
+        this.name = name;
+    }
+
+    public String getNamespace()
+    {
+        return namespace;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public List<Attribute> getAttributes() throws ODKDatastoreException
+    {
+        return getAttributes(namespace, name, relation.getCC());
+    }
+
+    private List<Attribute> getAttributes(String namespace, String name,
+            CallingContext cc) throws ODKDatastoreException
+    {
+        List<Attribute> attributes;
+        if (name.equals(AttributeRelation.name()))
+        {
+            attributes = AttributeRelation.attributes();
+        } else
+        {
+            // get attribute entities
+            List<Entity> attrEntities = getAttributeEntities(namespace, name,
+                    cc);
+
+            // convert Entities to Attributes
+            attributes = new ArrayList<Attribute>();
+            for (Entity attrEntity : attrEntities)
+            {
+                String attributeName = attrEntity
+                        .getString(AttributeRelation.NAME);
+                AttributeType attributeType = AttributeType.valueOf(attrEntity
+                        .getString(AttributeRelation.TYPE));
+                boolean attributeNullable = attrEntity
+                        .getBoolean(AttributeRelation.NULLABLE);
+                Attribute attribute = new Attribute(attributeName,
+                        attributeType, attributeNullable);
+                attributes.add(attribute);
+            }
+        }
+        return attributes;
+    }
+
+    private List<Entity> getAttributeEntities(String namespace, String name,
+            CallingContext cc) throws ODKDatastoreException
+    {
+        AttributeRelation attributeRelation = AttributeRelation.getInstance(
+                namespace, cc);
+        List<Entity> attrEntities = attributeRelation.query()
+                .equal(AttributeRelation.RELATION_NAMESPACE, namespace)
+                .equal(AttributeRelation.RELATION_NAME, name).execute();
+        return attrEntities;
     }
 
     public Entity getEntity(String aggregateIdentifier)
+            throws ODKEntityNotFoundException
     {
         Check.notNullOrEmpty(aggregateIdentifier, "aggregateIdentifier");
-        return Entity.fromEntity(this.relation.getEntity(aggregateIdentifier, this.relation.getCC()));
+        return Entity.fromEntity(relation,
+                relation.getEntity(aggregateIdentifier, relation.getCC()));
     }
 
     public Query query()
@@ -81,17 +166,30 @@ public class Relation
 
     public Entity newEntity()
     {
-        return Entity.fromEntity(this.relation.newEntity());
+        return Entity
+                .fromEntity(relation, relation.newEntity(relation.getCC()));
     }
 
-    public void dropRelation()
+    public void dropRelation() throws ODKDatastoreException
     {
-        this.relation.dropRelation();
+        for (Entity attribute : getAttributeEntities(namespace, name,
+                relation.getCC()))
+        {
+            attribute.delete();
+        }
+        try
+        {
+            relation.dropRelation(relation.getCC());
+        } catch (NullPointerException e)
+        {
+            throw new ODKDatastoreException(
+                    "Relation does not exist in datastore.", e);
+        }
     }
 
-    public static Relation getRelation(String namespace, String name, CallingContext cc)
+    public static Relation getRelation(String namespace, String name,
+            CallingContext cc) throws ODKDatastoreException
     {
         return new Relation(namespace, name, cc);
     }
 }
-
