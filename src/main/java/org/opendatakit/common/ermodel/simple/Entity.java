8135e838e53909e003655255faf9615af6291481
Dylan Price <the.dylan.price@gmail.com>
2011-07-21 22:31:33 -0700

    Updated data layer. Added getAttribute method to Relation and changed Entity set methods to be named 'set' instead of 'setString'.

--- src/main/java/org/opendatakit/common/ermodel/simple/Entity.java
+++ src/main/java/org/opendatakit/common/ermodel/simple/Entity.java
@@ -38,25 +38,11 @@ public class Entity
         return this.entity.getCreationDate();
     }
 
-    public void setBoolean(String attributeName, Boolean value)
-    {
-        Check.notNullOrEmpty(attributeName, "attributeName");
-        DataField field = relation.getDataField(attributeName);
-        this.entity.setBoolean(field, value);
-    }
-
     public Boolean getBoolean(String attributeName)
     {
         Check.notNullOrEmpty(attributeName, "attributeName");
         DataField field = relation.getDataField(attributeName);
         return this.entity.getBoolean(field);
-    }
-
-    public void setDate(String attributeName, Date value)
-    {
-        Check.notNullOrEmpty(attributeName, "attributeName");
-        DataField field = relation.getDataField(attributeName);
-        this.entity.setDate(field, value);
     }
 
     public Date getDate(String attributeName)
@@ -65,26 +51,13 @@ public class Entity
         DataField field = relation.getDataField(attributeName);
         return this.entity.getDate(field);
     }
-
-    public void setDouble(String attributeName, Double value)
-    {
-        Check.notNullOrEmpty(attributeName, "attributeName");
-        DataField field = relation.getDataField(attributeName);
-        this.entity.setDouble(field, value);
-    }
+    
 
     public Double getDouble(String attributeName)
     {
         Check.notNullOrEmpty(attributeName, "attributeName");
         DataField field = relation.getDataField(attributeName);
         return this.entity.getDouble(field);
-    }
-
-    public void setNumeric(String attributeName, BigDecimal value)
-    {
-        Check.notNullOrEmpty(attributeName, "attributeName");
-        DataField field = relation.getDataField(attributeName);
-        this.entity.setNumeric(field, value);
     }
 
     public BigDecimal getNumeric(String attributeName)
@@ -94,25 +67,11 @@ public class Entity
         return this.entity.getNumeric(field);
     }
 
-    public void setInteger(String attributeName, Integer value)
-    {
-        Check.notNullOrEmpty(attributeName, "attributeName");
-        DataField field = relation.getDataField(attributeName);
-        this.entity.setInteger(field, value);
-    }
-
     public Integer getInteger(String attributeName)
     {
         Check.notNullOrEmpty(attributeName, "attributeName");
         DataField field = relation.getDataField(attributeName);
         return this.entity.getInteger(field);
-    }
-
-    public void setLong(String attributeName, Long value)
-    {
-        Check.notNullOrEmpty(attributeName, "attributeName");
-        DataField field = relation.getDataField(attributeName);
-        this.entity.setLong(field, value);
     }
 
     public Long getLong(String attributeName)
@@ -122,18 +81,60 @@ public class Entity
         return this.entity.getLong(field);
     }
 
-    public void setString(String attributeName, String value)
-    {
-        Check.notNullOrEmpty(attributeName, "attributeName");
-        DataField field = relation.getDataField(attributeName);
-        this.entity.setString(field, value);
-    }
-
     public String getString(String attributeName)
     {
         Check.notNullOrEmpty(attributeName, "attributeName");
         DataField field = relation.getDataField(attributeName);
         return this.entity.getString(field);
+    }
+    
+    public void set(String attributeName, Boolean value)
+    {
+        Check.notNullOrEmpty(attributeName, "attributeName");
+        DataField field = relation.getDataField(attributeName);
+        this.entity.setBoolean(field, value);
+    }
+    
+    public void set(String attributeName, Date value)
+    {
+        Check.notNullOrEmpty(attributeName, "attributeName");
+        DataField field = relation.getDataField(attributeName);
+        this.entity.setDate(field, value);
+    }
+    
+    public void set(String attributeName, Double value)
+    {
+        Check.notNullOrEmpty(attributeName, "attributeName");
+        DataField field = relation.getDataField(attributeName);
+        this.entity.setDouble(field, value);
+    }
+
+    public void set(String attributeName, BigDecimal value)
+    {
+        Check.notNullOrEmpty(attributeName, "attributeName");
+        DataField field = relation.getDataField(attributeName);
+        this.entity.setNumeric(field, value);
+    }
+    
+    public void set(String attributeName, Integer value)
+    {
+        Check.notNullOrEmpty(attributeName, "attributeName");
+        DataField field = relation.getDataField(attributeName);
+        this.entity.setInteger(field, value);
+    }
+
+    public void set(String attributeName, Long value)
+    {
+        Check.notNullOrEmpty(attributeName, "attributeName");
+        DataField field = relation.getDataField(attributeName);
+        this.entity.setLong(field, value);
+    }
+
+    public void set(String attributeName, String value)
+    {
+        Check.notNullOrEmpty(attributeName, "attributeName");
+        DataField field = relation.getDataField(attributeName);
+        this.entity.setString(field, value);
     }
 
     public void save() throws ODKEntityPersistException
