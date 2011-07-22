a266e2a32094ed487ea55ea4d9ab4e45805bc82c
Dylan Price <the.dylan.price@gmail.com>
2011-07-22 01:19:46 -0700

    Updated RelationTest to test for retrieving an already existing relation. Updated EntityTest to test getAsString and setAsString.

--- src/test/java/org/opendatakit/common/ermodel/simple/EntityTest.java
+++ src/test/java/org/opendatakit/common/ermodel/simple/EntityTest.java
@@ -42,6 +42,32 @@ public class EntityTest
     }
 
     @Test
+    public void testSet() throws ODKEntityNotFoundException,
+            ODKEntityPersistException
+    {
+        Entity john = d.relation.getEntity(d.johnsIdentifier);
+        john.set(d.attrName, "John2");
+        john.set(d.attrAge, 100);
+        john.save();
+        john = null;
+        john = d.relation.getEntity(d.johnsIdentifier);
+        assertEquals("John2", john.getString(d.attrName));
+        assertEquals(100, john.getInteger(d.attrAge).intValue());
+    }
+
+    @Test
+    public void testSetAsString() throws ODKEntityNotFoundException,
+            ODKEntityPersistException
+    {
+        Entity john = d.relation.getEntity(d.johnsIdentifier);
+        john.setAsString(d.attrAge, "100");
+        john.save();
+        john = null;
+        john = d.relation.getEntity(d.johnsIdentifier);
+        assertEquals(100, john.getInteger(d.attrAge).intValue());
+    }
+
+    @Test
     public void testDeleteEntity() throws ODKDatastoreException
     {
         Entity john = d.relation.getEntity(d.johnsIdentifier);
