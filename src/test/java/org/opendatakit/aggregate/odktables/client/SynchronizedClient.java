5c63a02ba935becb81d9c0528942b9d6852f3290
Dylan Price <the.dylan.price@gmail.com>
2011-08-04 02:26:08 -0700

    Checking in test driver and one test script.

--- src/test/java/org/opendatakit/aggregate/odktables/client/SynchronizedClient.java
+++ src/test/java/org/opendatakit/aggregate/odktables/client/SynchronizedClient.java
@@ -1,34 +1,64 @@
 package org.opendatakit.aggregate.odktables.client;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * Simulates a client of the odktables API with functionality for storing
+ * synchronized tables.
+ * 
+ * @author the.dylan.price@gmail.com
+ * 
+ */
 public class SynchronizedClient
 {
-    
+
     private String clientName;
+    private String aggregateUserIdentifier;
     // tableID to SynchronizedTable
     private Map<String, SynchronizedTable> tables;
-    
+
     public SynchronizedClient(String clientName)
     {
         this.clientName = clientName;
+        this.aggregateUserIdentifier = null;
         tables = new HashMap<String, SynchronizedTable>();
     }
-    
-    public void addTable(SynchronizedTable table)
+
+    public String getClientName()
     {
-       tables.put(table.getTableID(), table);
+        return clientName;
     }
 
-    public void getTable(String tableID)
+    public void setAggregateUserIdentifier(String value)
+    {
+        this.aggregateUserIdentifier = value;
+    }
+
+    public String getAggregateUserIdentifier()
+    {
+        return this.aggregateUserIdentifier;
+    }
+
+    public void addTable(SynchronizedTable table)
+    {
+        tables.put(table.getTableName(), table);
+    }
+
+    public void removeTable(String tableID)
+    {
+        tables.remove(tableID);
+    }
+
+    public SynchronizedTable getTable(String tableID)
     {
         return tables.get(tableID);
     }
-    
-    public List<SynchronizedTable> getTables()
+
+    public Collection<SynchronizedTable> getTables()
     {
-        return tables.valueSet();
+        return tables.values();
     }
 
 }
