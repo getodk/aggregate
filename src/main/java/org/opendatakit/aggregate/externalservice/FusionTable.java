e2577a76925ad78424b4f81737234eef44625bef
msundt <msundt@cs.washington.edu>
2011-08-24 16:24:57 -0700

    Increase fusion table connection timeout values to much higher values.

--- src/main/java/org/opendatakit/aggregate/externalservice/FusionTable.java
+++ src/main/java/org/opendatakit/aggregate/externalservice/FusionTable.java
@@ -97,8 +97,8 @@ import com.google.gdata.util.ServiceException;
 public class FusionTable extends AbstractExternalService implements ExternalService {
   private static final Log logger = LogFactory.getLog(FusionTable.class.getName());
 
-  private static final int FUSION_TABLE_SOCKET_ESTABLISHMENT_TIMEOUT_MILLISECONDS = 2000;
-  private static final int FUSION_TABLE_SERVICE_TIMEOUT_MILLISECONDS = 15000;
+  private static final int FUSION_TABLE_SOCKET_ESTABLISHMENT_TIMEOUT_MILLISECONDS = 30000;
+  private static final int FUSION_TABLE_SERVICE_TIMEOUT_MILLISECONDS = 30000;
 
   /**
    * Datastore entity specific to this type of external service
