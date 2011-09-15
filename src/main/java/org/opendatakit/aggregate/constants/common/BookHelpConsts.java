cdd6f95af88d9de84bb0ba5b8720922899d45fc0
wbrunette <devnull@localhost>
2011-09-15 08:45:31 -0700

    UI tweaks

--- src/main/java/org/opendatakit/aggregate/constants/common/BookHelpConsts.java
+++ src/main/java/org/opendatakit/aggregate/constants/common/BookHelpConsts.java
@@ -3,24 +3,25 @@ package org.opendatakit.aggregate.constants.common;
 public enum BookHelpConsts {
 
 	XFORM("Forms", 
+	      "http://www.cs.washington.edu/homes/wrb/NewForm.mpg" , 
 			"Forms are tables which organize your data into rows and columns.  You can view your " +
 			"forms on \"Submissions\" -> \"Filter Submissions\".<br><br>" +
 			"What is \"Downloadable\"? (\"Form Management\" -> \"Forms List\")<br>" +
 			"&nbsp;This setting allows remote clients to download the form. <br><br>" +
 			"What is \"Accept Submissions\"? (\"Form Management\" -> \"Forms List\")<br>" +
-			"&nbsp;This setting allows the form to accept data while it is in Aggregate.", 
-			"How do I add a new form? (Press \"New Form\" on \"Form Management\" -> \"Forms List\")<br>" +
+			"&nbsp;This setting allows the form to accept data while it is in Aggregate.", "How do I add a new form? (Press \"New Form\" on \"Form Management\" -> \"Forms List\")<br>" +
 			"&nbsp;1.  Form Definition - required.  Please choose the .xml file that will be used.<br>" +
 			"&nbsp;2.  Media File(s) - optional.  Choose the appropriate media files for the form."),
 	
 	SUBMISSIONS("Submissions", 
+			null, 
 			"Submissions are the data contained in a form.  One submission corresponds to one row in the " +
-			"table.", 
-			"How do I upload data? (Press \"Upload Data\" on \"Form Management\" -> \"Forms List\")<br>" +
+			"table.", "How do I upload data? (Press \"Upload Data\" on \"Form Management\" -> \"Forms List\")<br>" +
 			"&nbsp;1.  Form Definition - required.  Please choose the .xml file that will be used.<br>" +
 	"&nbsp;2.  Media File(s) - optional.  Choose the appropriate media files for the form."),
 	
 	FILTER("Filters", 
+	      "http://www.cs.washington.edu/homes/wrb/Filter.mpg", 
 			"Filters give you the ability to see a subset of your data.  For example, if you wanted to " +
 			"only look at males over the age of 35, you would include 2 filters: <br>" +
 			"&nbsp;1.  Display Rows where column Gender EQUAL male <br>" +
@@ -28,8 +29,7 @@ public enum BookHelpConsts {
 			"If you have multiple filters applied at once, then you have a filter group. <br><br>" +
 			"If you were more curious about viewing the distribution of locations, you could do one " +
 			"filter:<br>" +
-			"&nbsp;1.  Display columns location", 
-			"How do I add a filter? (Press \"Add Filter\" on " +
+			"&nbsp;1.  Display columns location", "How do I add a filter? (Press \"Add Filter\" on " +
 			"\"Submissions\" -> \"Filter Submissions\") <br>" +
 			"&nbsp;1.  Display/Hide - Will you be selecting data to show or hide?<br>" +
 			"&nbsp;2.  Rows/Columns - Choose whether you will be working with the rows or columns of the table.<br>" +
@@ -46,16 +46,16 @@ public enum BookHelpConsts {
 	"&nbsp;2.  Press \"OK\"."),
 	
 	METADATA("Metadata", 
+			null, 
 			"Metadata provides information about the submissions being submitted.  There will be " +
 			"information such as date submitted, if the data is complete, version numbers, " +
-			"and id numbers", 
-			"You can toggle whether or not you view metadata by clicking the \"Display Metadata\"" +
+			"and id numbers", "You can toggle whether or not you view metadata by clicking the \"Display Metadata\"" +
 			"checkbox on \"Submissions\" -> \"Filter Submissions\"."),
 	
 	VISUALIZE("Visualize", 
+	    "http://www.cs.washington.edu/homes/wrb/Visualization.mpg",
 			"Visualizing the data is a quick way to see early results.  You can view your data in a bar" +
-			"graph, pie chart, or on a map.",
-			"How do I visualize the data? (Press \"Visualize\" on \"Submissions\" -> \"Filter Submissions\" " +
+			"graph, pie chart, or on a map.", "How do I visualize the data? (Press \"Visualize\" on \"Submissions\" -> \"Filter Submissions\" " +
 			"or on \"Form Management\" -> \"Forms List\")<br>" +
 			"&nbsp;1.  Choose whether you want to view a Pie Chart, Bar Graph, or a Map.<br>" +
 			"&nbsp;2a.  If you choose Pie Chart:<br>" +
@@ -74,10 +74,10 @@ public enum BookHelpConsts {
 	"&nbsp;&nbsp;c.  Press \"Map It\"."),
 	
 	EXPORT("Export", 
+	    "http://www.cs.washington.edu/homes/wrb/Export.mpg",
 			"Exporting your data allows you to view the information in another software tool.  This" +
 			"can allow you to do more comprehensive work with your data.  You can export your file to" +
-			"a CSV file (Excel) or a KML file (Google Maps).",
-			"How do I export a file? (Press \"Export\" on \"Submissions\" -> \"Filter Submissions\" " +
+			"a CSV file (Excel) or a KML file (Google Maps).", "How do I export a file? (Press \"Export\" on \"Submissions\" -> \"Filter Submissions\" " +
 			"or on \"Form Management\" -> \"Forms List\")<br>" +
 			"&nbsp;1.  Choose whether you want to export to a .csv file, or a .kml file.<br>" +
 			"&nbsp;2a.  If you choose CSV, just press \"Export\".<br>" +
@@ -88,10 +88,10 @@ public enum BookHelpConsts {
 	"&nbsp;&nbsp;d.  Press \"Export\"."),
 	
 	PUBLISH("Publish",
+	    "http://www.cs.washington.edu/homes/wrb/Publish.mpg", 
 			"When you publish your data, it will be uploaded to a software tool: Google Fusion Tables or " +
 			"Google Spreadsheets.  You can display data before a certain time, after a certain time, or " +
-			"stream all of the data.  This will allow others to work with the data to do analysis.", 
-			"How do I publish my data? (Press \"Publish\" on \"Submissions\" -> \"Filter Submissions\" " +
+			"stream all of the data.  This will allow others to work with the data to do analysis.", "How do I publish my data? (Press \"Publish\" on \"Submissions\" -> \"Filter Submissions\" " +
 			"or on \"Form Management\" -> \"Forms List\")<br>" +
 			"&nbsp;1.  Choose whether you want your data in a Google Fusion Table or a Google Spreadsheet.<br>" +
 			"&nbsp;2a.  If you chose Google Fusion Table:<br>" +
@@ -113,15 +113,17 @@ public enum BookHelpConsts {
 	"&nbsp;&nbsp;e.  You can view your published document in Google Docs."),
 	
 	PERMISSIONS("Permissions", 
-			"", 
-			"");
+			null, 
+			"", "");
 	
 	private String title;
 	private String concept;
 	private String procedures;
+	private String videoUrl;
 
-	private BookHelpConsts(String title, String concept, String procedures) {
+	private BookHelpConsts(String title, String helpVidUrl, String concept, String procedures) {
 		this.title = title;
+		this.videoUrl = helpVidUrl;
 		this.concept = concept;
 		this.procedures = procedures;
 	}
@@ -136,5 +138,9 @@ public enum BookHelpConsts {
 
 	public String getProcedures() {
 		return procedures;
+	}
+	
+	public String getVideoUrl() {
+	  return videoUrl;
 	}
 }
