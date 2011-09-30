47d921f504a8305871d9edbd5271f2216573a0da
msundt <msundt@cs.washington.edu>
2011-09-30 11:43:07 -0700

    Adding priorResults flag and resume point sense.

--- src/main/java/org/opendatakit/common/persistence/QueryResult.java
+++ src/main/java/org/opendatakit/common/persistence/QueryResult.java
@@ -17,52 +17,61 @@ import java.util.List;
 
 /**
  * Result object that understands and supports resumable queries.
- * <p>Once a query object is constructed, it can be repeatedly invoked
- * with the resumeCursor from any QueryResult to return the next set
- * results from that query.  
- * <p>Alternatively, the user can obtain the elements preceding the 
- * contents of the result set by constructing a 'backward query' with
- * the same filter criteria but all sort directions inverted and 
- * pass the backwardCursor defined in this QueryResult into that 
- * new query object to obtain the preceding elements.
+ * <p>
+ * Once a query object is constructed, it can be repeatedly invoked with the
+ * resumeCursor from any QueryResult to return the next set results from that
+ * query.
+ * <p>
+ * Alternatively, the user can obtain the elements preceding the contents of the
+ * result set by constructing a 'backward query' with the same filter criteria
+ * but all sort directions inverted and pass the backwardCursor defined in this
+ * QueryResult into that new query object to obtain the preceding elements.
  * 
  * @author mitchellsundt@gmail.com
- *
+ * 
  */
 public class QueryResult {
 
-	private final QueryResumePoint startCursor;
-	private final QueryResumePoint backwardCursor;
-	private final QueryResumePoint resumeCursor;
-	private final boolean moreResults;
-	
-	private final List<? extends CommonFieldsBase> resultList;
-	
-	public QueryResult(QueryResumePoint startCursor, List<? extends CommonFieldsBase> resultList, QueryResumePoint backwardCursor, QueryResumePoint resumeCursor, boolean moreResults) {
-		this.startCursor = startCursor;
-		this.resultList = resultList;
-		this.backwardCursor = backwardCursor;
-		this.resumeCursor = resumeCursor;
-		this.moreResults = moreResults;
-	}
+  private final QueryResumePoint startCursor;
+  private final QueryResumePoint backwardCursor;
+  private final QueryResumePoint resumeCursor;
+  private final boolean hasMoreResults;
+  private final boolean hasPriorResults;
 
-	public QueryResumePoint getResumeCursor() {
-		return resumeCursor;
-	}
+  private final List<? extends CommonFieldsBase> resultList;
 
-	public List<? extends CommonFieldsBase> getResultList() {
-		return resultList;
-	}
+  public QueryResult(QueryResumePoint startCursor, List<? extends CommonFieldsBase> resultList,
+      QueryResumePoint backwardCursor, QueryResumePoint resumeCursor, boolean moreResults) {
+    this.startCursor = startCursor;
+    this.resultList = resultList;
+    this.backwardCursor = backwardCursor;
+    this.resumeCursor = resumeCursor;
+    this.hasMoreResults = moreResults;
+    this.hasPriorResults = (startCursor != null);
+  }
 
-	public QueryResumePoint getStartCursor() {
-		return startCursor;
-	}
-	
-	public QueryResumePoint getBackwardCursor() {
-		return backwardCursor;
-	}
-	
-	public boolean hasMoreResults() {
-		return moreResults;
-	}
+  public QueryResumePoint getResumeCursor() {
+    return resumeCursor;
+  }
+
+  public List<? extends CommonFieldsBase> getResultList() {
+    return resultList;
+  }
+
+  public QueryResumePoint getStartCursor() {
+    return startCursor;
+  }
+
+  public QueryResumePoint getBackwardCursor() {
+    return backwardCursor;
+  }
+
+  public boolean hasMoreResults() {
+    return hasMoreResults;
+  }
+
+  public boolean hasPriorResults() {
+    return hasPriorResults;
+  }
+
 }
