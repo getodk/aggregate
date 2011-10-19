91e95c9969cbe256681ce00ec77cb43e320f7325
msundt <msundt@cs.washington.edu>
2011-10-19 11:04:59 -0700

    Functioning Selenium 2 integration tests under Maven.

--- src/it/java/org/opendatakit/aggregate/integration/TestStartPage.java
+++ src/it/java/org/opendatakit/aggregate/integration/TestStartPage.java
@@ -1,25 +1,73 @@
 package org.opendatakit.aggregate.integration;
 
-import com.thoughtworks.selenium.SeleneseTestCase;
+import static org.junit.Assert.assertEquals;
 
-public class TestStartPage extends SeleneseTestCase 
-{
-	private String baseUrl;
-	private int port;
+import java.util.concurrent.TimeUnit;
 
-	public void setUp() throws Exception
-	{
-		baseUrl = System.getProperty("project.artifactId");
-		port = Integer.parseInt(System.getProperty("test.server.port"));
-		// We should also test different browsers?
-		setUp("http://localhost:" + port + "/" + baseUrl + "/", "*chrome");
-	}
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverBackedSelenium;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-	public void testStartPageHasCorrectTitle() throws Exception
-	{
-		selenium.open("forms");
-		assertEquals("ODK AGGREGATE", selenium.getTitle());
-		assertEquals("Name", selenium.getText("//th[1]"));
-		assertTrue(selenium.isElementPresent("link=List Forms"));
-	}
+import com.thoughtworks.selenium.Selenium;
+import com.thoughtworks.selenium.Wait;
+
+@RunWith(org.junit.runners.JUnit4.class)
+public class TestStartPage {
+  private static long TIMEOUT_INTERVAL_MS = 30000L; // ms
+  private static long RETRY_INTERVAL_MS = 100L; // ms
+
+  private static String formsDir;
+  private static String hostname;
+  private static String baseUrl;
+  private static String username = "aggregate";
+  private static String password = "aggregate";
+  private static int port;
+  private static WebDriver driver;
+  private static Selenium selenium;
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    formsDir = System.getProperty("test.forms.dir");
+    hostname = System.getProperty("test.server.hostname");
+    baseUrl = System.getProperty("test.server.baseUrl");
+    port = Integer.parseInt(System.getProperty("test.server.port"));
+    // We should also test different browsers?
+    driver = new FirefoxDriver();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    selenium = new WebDriverBackedSelenium(driver, "http://" + username + ":" + password + "@"
+        + hostname + ":" + port + baseUrl);
+
+    selenium.open("local_login.html");
+    Wait mainload = new Wait() {
+      public boolean until() {
+        return selenium.isTextPresent("Form Management");
+      }
+    };
+    mainload.wait("Login did not progress to Aggregate.html page", TIMEOUT_INTERVAL_MS,
+        RETRY_INTERVAL_MS);
+  }
+
+  @Test
+  public void testStartPageHasCorrectTitle() throws Exception {
+    selenium.open("Aggregate.html");
+    Wait mainload = new Wait() {
+      public boolean until() {
+        return selenium.isTextPresent("Form Management");
+      }
+    };
+    mainload.wait("Login did not progress to Aggregate.html page", TIMEOUT_INTERVAL_MS,
+        RETRY_INTERVAL_MS);
+    assertEquals("ODK Aggregate", selenium.getTitle());
+    // assertEquals("Name", selenium.getText("//th[1]"));
+    // assertTrue(selenium.isElementPresent("link=List Forms"));
+  }
+
+  @AfterClass
+  public static void tearDown() throws Exception {
+    selenium.close();
+  }
 }
