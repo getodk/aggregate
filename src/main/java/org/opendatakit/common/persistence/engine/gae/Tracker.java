b3825e50ef81317b19167465f32cb1a6ced33151
mitchellsundt <mitchellsundt@gmail.com>
2011-11-29 09:30:00 -0800

    Move helper classes and interfaces out of GAE QueryImpl. Clean up coreExecuteQuery() implementation and improve out-of-memory error handling.  Eliminate filter reordering. Reduce record fetch limit on query (to minimize risk of time-outs).

---
+++ src/main/java/org/opendatakit/common/persistence/engine/gae/Tracker.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (C) 2011 University of Washington
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.opendatakit.common.persistence.engine.gae;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.opendatakit.common.persistence.CommonFieldsBase;
+import org.opendatakit.common.persistence.DataField;
+
+/**
+ * Track the attributes that we are querying and sorting on...
+ * 
+ * @author mitchellsundt@gmail.com
+ * 
+ */
+abstract class Tracker {
+  final DataField attribute;
+
+  Tracker(DataField attribute) {
+    this.attribute = attribute;
+  }
+
+  DataField getAttribute() {
+    return attribute;
+  }
+
+  public int simpleValueCompare(CommonFieldsBase b1, CommonFieldsBase b2) {
+    Object value;
+    switch (attribute.getDataType()) {
+    default:
+      throw new IllegalStateException("missing dataType implementation");
+    case BINARY:
+    case LONG_STRING:
+      throw new IllegalStateException("should never filter on large objects (text or blob)");
+    case STRING:
+    case URI:
+      value = b2.getStringField(attribute);
+      break;
+    case INTEGER:
+      value = b2.getLongField(attribute);
+      break;
+    case DECIMAL:
+      value = b2.getNumericField(attribute);
+      break;
+    case BOOLEAN:
+      value = b2.getBooleanField(attribute);
+      break;
+    case DATETIME:
+      value = b2.getDateField(attribute);
+      break;
+    }
+    return compareField(b1, value);
+  }
+
+  <T extends Comparable<T>> int compareObjects(T b1, T b2) {
+    if (b1 == null) {
+      if (b2 == null)
+        return 0;
+      return 1; // nulls (==b2) appear last in ordering
+    }
+    if (b2 == null)
+      return -1; // nulls (==b1) appear last in ordering
+    return b1.compareTo(b2);
+  }
+
+  int compareField(CommonFieldsBase record, Object value) {
+    switch (attribute.getDataType()) {
+    default:
+      throw new IllegalStateException("missing dataType implementation");
+    case BINARY:
+    case LONG_STRING:
+      throw new IllegalStateException("should never filter on large objects (text or blob)");
+    case STRING:
+    case URI:
+      String eStr = record.getStringField(attribute);
+      String vStr = (value == null) ? null : (String) value;
+      return compareObjects(eStr, vStr);
+    case INTEGER:
+      Long eLong = record.getLongField(attribute);
+      Long vLong;
+      if (value == null) {
+        vLong = null;
+      } else if (value instanceof Long) {
+        vLong = (Long) value;
+      } else {
+        vLong = Long.parseLong(value.toString());
+      }
+      return compareObjects(eLong, vLong);
+    case DECIMAL:
+      BigDecimal eDec = record.getNumericField(attribute);
+      BigDecimal vDec;
+      if (value == null) {
+        vDec = null;
+      } else {
+        vDec = (BigDecimal) value;
+      }
+      return compareObjects(eDec, vDec);
+    case BOOLEAN:
+      Boolean eBool = record.getBooleanField(attribute);
+      Boolean vBool = (value == null) ? null : (Boolean) value;
+      return compareObjects(eBool, vBool);
+    case DATETIME:
+      Date eDate = record.getDateField(attribute);
+      Date vDate = (value == null) ? null : (Date) value;
+      return compareObjects(eDate, vDate);
+    }
+  }
+
+  abstract boolean passFilter(CommonFieldsBase record);
+
+  abstract void setFilter(com.google.appengine.api.datastore.Query q);
+}
