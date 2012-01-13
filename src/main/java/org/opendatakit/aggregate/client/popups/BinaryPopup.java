19239e7393a8938d642c0a32e6d330cb37eb2ee3
mitchellsundt <mitchellsundt@gmail.com>
2012-01-13 09:33:43 -0800

    Define and apply verticalFlowPanel stylename everywhere a VerticalPanel was replaced with FlowPanel. Update to 1.0.4.

--- src/main/java/org/opendatakit/aggregate/client/popups/BinaryPopup.java
+++ src/main/java/org/opendatakit/aggregate/client/popups/BinaryPopup.java
@@ -17,6 +17,7 @@
 package org.opendatakit.aggregate.client.popups;
 
 import org.opendatakit.aggregate.client.widgets.ClosePopupButton;
+import org.opendatakit.aggregate.constants.common.UIConsts;
 
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -41,6 +42,7 @@ public class BinaryPopup extends AbstractPopupBase {
     frame.setPixelSize(width, height); 
 
     FlowPanel panel = new FlowPanel();
+    panel.setStylePrimaryName(UIConsts.VERTICAL_FLOW_PANEL_STYLENAME);
     panel.setPixelSize(width+6, height+30);
     panel.add(new SimplePanel(new ClosePopupButton(this)));       
     panel.add(frame);      
