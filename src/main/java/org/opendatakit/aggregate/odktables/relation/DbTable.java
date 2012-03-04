b4a3f653805bdaf7a1e91a614ff8da946303169e
Dylan Price <the.dylan.price@gmail.com>
2012-03-04 01:39:43 -0800

    Added query based on rowIds.

--- src/main/java/org/opendatakit/aggregate/odktables/relation/DbTable.java
+++ src/main/java/org/opendatakit/aggregate/odktables/relation/DbTable.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.lang.Validate;
 import org.opendatakit.common.ermodel.simple.Entity;
+import org.opendatakit.common.ermodel.simple.Query;
 import org.opendatakit.common.ermodel.simple.Relation;
+import org.opendatakit.common.persistence.CommonFieldsBase;
 import org.opendatakit.common.persistence.DataField;
 import org.opendatakit.common.persistence.DataField.DataType;
 import org.opendatakit.common.persistence.exception.ODKDatastoreException;
+import org.opendatakit.common.persistence.exception.ODKEntityNotFoundException;
 import org.opendatakit.common.web.CallingContext;
 
 public class DbTable {
@@ -29,17 +33,17 @@ public class DbTable {
 
   private static final EntityConverter converter = new EntityConverter();
 
-  public static Relation getRelation(String tableId, List<DataField> fields, CallingContext cc)
-      throws ODKDatastoreException {
-    Relation relation = new Relation(RUtil.NAMESPACE, RUtil.convertIdentifier(tableId), fields, cc);
-    return relation;
-  }
-
   public static Relation getRelation(String tableId, CallingContext cc)
       throws ODKDatastoreException {
     List<DataField> fields = getDynamicFields(tableId, cc);
     fields.addAll(getStaticFields());
     return getRelation(tableId, fields, cc);
+  }
+
+  private static Relation getRelation(String tableId, List<DataField> fields, CallingContext cc)
+      throws ODKDatastoreException {
+    Relation relation = new Relation(RUtil.NAMESPACE, RUtil.convertIdentifier(tableId), fields, cc);
+    return relation;
   }
 
   private static List<DataField> getDynamicFields(String tableId, CallingContext cc)
@@ -50,6 +54,31 @@ public class DbTable {
 
   private static List<DataField> getStaticFields() {
     return Collections.unmodifiableList(dataFields);
+  }
+
+  /**
+   * Retrieve a list of {@link DbTable} row entities.
+   * 
+   * @param table
+   *          the {@link DbTable} relation.
+   * @param rowIds
+   *          the ids of the rows to get.
+   * @param cc
+   * @return
+   * @throws ODKEntityNotFoundException
+   *           if one of the rows does not exist
+   * @throws ODKDatastoreException
+   */
+  public static List<Entity> query(Relation table, List<String> rowIds, CallingContext cc)
+      throws ODKEntityNotFoundException, ODKDatastoreException {
+    Validate.notNull(table);
+    Validate.noNullElements(rowIds);
+    Validate.notNull(cc);
+
+    Query query = table.query("DbTable.query", cc);
+    query.include(CommonFieldsBase.URI_COLUMN_NAME, rowIds);
+    List<Entity> entities = query.execute();
+    return entities;
   }
 
 }
