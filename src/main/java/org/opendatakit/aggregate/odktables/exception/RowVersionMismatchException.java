d22b712a131beb082d4b171870fe130e00e63874
the dylan price <the.dylan.price@gmail.com>
2012-03-05 02:21:04 -0800

    Added ODKTablesException and changed RowVersionMismatchException to extend.

--- src/main/java/org/opendatakit/aggregate/odktables/exception/RowVersionMismatchException.java
+++ src/main/java/org/opendatakit/aggregate/odktables/exception/RowVersionMismatchException.java
@@ -1,6 +1,6 @@
 package org.opendatakit.aggregate.odktables.exception;
 
-public class RowVersionMismatchException extends Exception {
+public class RowVersionMismatchException extends ODKTablesException {
 
   private static final long serialVersionUID = 1L;
 
