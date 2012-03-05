c14dd80e37f2864dd95329501e89d449b7719cb3
Dylan Price <the.dylan.price@gmail.com>
2012-03-05 03:23:35 -0800

    Added application.

---
+++ src/main/java/org/opendatakit/aggregate/odktables/api/ODKTablesAPIApplication.java
@@ -0,0 +1,9 @@
+package org.opendatakit.aggregate.odktables.api;
+
+import com.sun.jersey.api.core.PackagesResourceConfig;
+
+public class ODKTablesAPIApplication extends PackagesResourceConfig {
+  public ODKTablesAPIApplication() {
+    super("org.opendatakit.aggregate.odktables.api");
+  }
+}
