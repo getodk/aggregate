cb7f973891cbbaa73d80afe9071f35afc2933223
the dylan price <the.dylan.price@gmail.com>
2012-03-05 18:06:22 -0800

    Fixed imports and added constructor.

--- src/main/java/org/opendatakit/aggregate/odktables/api/entity/RowResource.java
+++ src/main/java/org/opendatakit/aggregate/odktables/api/entity/RowResource.java
@@ -4,14 +4,29 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import org.opendatakit.aggregate.odktables.entity.Row;
 
 @XmlRootElement
+@NoArgsConstructor
 @Data
+@ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class RowResource extends Row {
-  
+
   private String selfUri;
+  private String tableUri;
   
+  public RowResource(Row row)
+  {
+    super();
+    setRowId(row.getRowId());
+    setRowEtag(row.getRowEtag());
+    setGroupOrUserId(row.getGroupOrUserId());
+    setDeleted(row.isDeleted());
+    setValues(row.getValues());
+  }
+
 }
