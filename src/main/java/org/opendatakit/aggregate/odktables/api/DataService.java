3303aeedfa936e81ad165e80bb5f535eaa0ec664
the dylan price <the.dylan.price@gmail.com>
2012-03-05 18:05:48 -0800

    Added DataService.

---
+++ src/main/java/org/opendatakit/aggregate/odktables/api/DataService.java
@@ -0,0 +1,41 @@
+package org.opendatakit.aggregate.odktables.api;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.opendatakit.aggregate.odktables.api.entity.RowResource;
+import org.opendatakit.aggregate.odktables.entity.Row;
+import org.opendatakit.aggregate.odktables.exception.RowVersionMismatchException;
+import org.opendatakit.common.persistence.exception.ODKDatastoreException;
+import org.opendatakit.common.persistence.exception.ODKTaskLockException;
+
+@Produces(MediaType.TEXT_XML)
+public interface DataService {
+
+  @GET
+  public List<RowResource> getRows() throws ODKDatastoreException;
+
+  @GET
+  @Path("{rowId}")
+  public RowResource getRow(@PathParam("rowId") String rowId) throws ODKDatastoreException;
+
+  @PUT
+  @Path("{rowId}")
+  @Consumes(MediaType.TEXT_XML)
+  public RowResource createOrUpdateRow(@PathParam("rowId") String rowId, Row row)
+      throws ODKTaskLockException, ODKDatastoreException, RowVersionMismatchException;
+
+  @DELETE
+  @Path("{rowId}")
+  public void deleteRow(@PathParam("rowId") String rowId) throws ODKDatastoreException,
+      ODKTaskLockException;
+
+}
