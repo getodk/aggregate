94cb7ec0cdfc9d52a8d617b62bd751c02bf19e0b
the dylan price <the.dylan.price@gmail.com>
2012-03-05 23:50:50 -0800

    Added Util.

---
+++ src/it/java/org/opendatakit/aggregate/integration/utilities/Util.java
@@ -0,0 +1,30 @@
+package org.opendatakit.aggregate.integration.utilities;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Util {
+
+  /**
+   * V must implement equals.
+   */
+  public static <V> void assertCollectionSameElements(Collection<V> expected, Collection<V> actual) {
+    Collection<V> expectedCopy = new ArrayList<V>(expected);
+    Collection<V> actualCopy = new ArrayList<V>(actual);
+
+    for (V item : actualCopy) {
+      assertTrue(expectedCopy.remove(item));
+    }
+    assertTrue(expectedCopy.isEmpty());
+  }
+
+  public static <V> List<V> list(V... values) {
+    ArrayList<V> list = new ArrayList<V>();
+    for (V value : values) {
+      list.add(value);
+    }
+    return list;
+  }
+}
