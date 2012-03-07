f9e99eb79c54b8b845fa866a2edea69a87e57ff8
Dylan Price <the.dylan.price@gmail.com>
2012-03-07 01:18:15 -0800

    Added @Provider annotation.

--- src/main/java/org/opendatakit/aggregate/odktables/impl/api/RuntimeExceptionMapper.java
+++ src/main/java/org/opendatakit/aggregate/odktables/impl/api/RuntimeExceptionMapper.java
@@ -3,7 +3,9 @@ package org.opendatakit.aggregate.odktables.impl.api;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
+@Provider
 public class RuntimeExceptionMapper implements ExceptionMapper<RuntimeException> {
 
   @Override
