fae1b7c42b085029dde25556c8d2c0c1adaa6027
Dylan Price <the.dylan.price@gmail.com>
2012-03-06 22:39:51 -0800

    Converted entities to xstream.

--- src/main/java/org/opendatakit/aggregate/odktables/entity/api/TableDefinition.java
+++ src/main/java/org/opendatakit/aggregate/odktables/entity/api/TableDefinition.java
@@ -1,37 +1,18 @@
 package org.opendatakit.aggregate.odktables.entity.api;
 
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
+
+import lombok.Data;
 
 import org.opendatakit.aggregate.odktables.entity.Column;
 
-@XmlRootElement
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
+
+@XStreamAlias("tableDefinition")
+@Data
 public class TableDefinition {
+  
+  @XStreamImplicit(itemFieldName="column")
   private List<Column> columns;
-
-  public TableDefinition() {
-  }
-
-  /**
-   * @param columns
-   */
-  public TableDefinition(List<Column> columns) {
-    this.columns = columns;
-  }
-
-  /**
-   * @return the columns
-   */
-  public List<Column> getColumns() {
-    return columns;
-  }
-
-  /**
-   * @param columns
-   *          the columns to set
-   */
-  public void setColumns(List<Column> columns) {
-    this.columns = columns;
-  }
-
 }
