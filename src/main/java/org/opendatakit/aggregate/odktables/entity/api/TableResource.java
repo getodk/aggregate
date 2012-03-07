d55aa9ded873b61fda23f8b1eb5a8bfa748ebb32
Dylan Price <the.dylan.price@gmail.com>
2012-03-07 01:17:31 -0800

    Formatted.

--- src/main/java/org/opendatakit/aggregate/odktables/entity/api/TableResource.java
+++ src/main/java/org/opendatakit/aggregate/odktables/entity/api/TableResource.java
@@ -2,26 +2,29 @@ package org.opendatakit.aggregate.odktables.entity.api;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import org.opendatakit.aggregate.odktables.entity.TableEntry;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 
 @XStreamAlias("tableResource")
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 @Data
 public class TableResource extends TableEntry {
 
-  @XStreamAlias("self")
+  @XStreamAsAttribute
   private String selfUri;
 
-  @XStreamAlias("columns")
+  @XStreamAsAttribute
   private String columnsUri;
 
-  @XStreamAlias("data")
+  @XStreamAsAttribute
   private String dataUri;
 
-  @XStreamAlias("diff")
+  @XStreamAsAttribute
   private String diffUri;
 
   public TableResource(TableEntry entry) {
