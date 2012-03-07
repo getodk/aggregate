b65f7e40e162aa79cb8d51ee9d27bccda4b01ef8
Dylan Price <the.dylan.price@gmail.com>
2012-03-07 01:17:31 -0800

    Formatted.

--- src/main/java/org/opendatakit/aggregate/odktables/entity/api/RowResource.java
+++ src/main/java/org/opendatakit/aggregate/odktables/entity/api/RowResource.java
@@ -2,20 +2,23 @@ package org.opendatakit.aggregate.odktables.entity.api;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import org.opendatakit.aggregate.odktables.entity.Row;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 
 @XStreamAlias("rowResource")
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 @Data
 public class RowResource extends Row {
 
-  @XStreamAlias("self")
+  @XStreamAsAttribute
   private String selfUri;
 
-  @XStreamAlias("table")
+  @XStreamAsAttribute
   private String tableUri;
 
   public RowResource(Row row) {
