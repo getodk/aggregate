ebaa9eea5655310f3b076c867ba384affe3ff21c
the dylan price <the.dylan.price@gmail.com>
2012-03-09 20:25:01 -0800

    Updated list of classes.

--- src/main/java/org/opendatakit/aggregate/odktables/impl/api/ODKTablesAPIApplication.java
+++ src/main/java/org/opendatakit/aggregate/odktables/impl/api/ODKTablesAPIApplication.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
-import org.opendatakit.aggregate.odktables.entity.serialization.XStreamAliasProvider;
+import org.opendatakit.aggregate.odktables.entity.serialization.SimpleXMLMessageReaderWriter;
 
 
 public class ODKTablesAPIApplication extends Application {
@@ -13,7 +13,7 @@ public class ODKTablesAPIApplication extends Application {
   public Set<Class<?>> getClasses() {
     final java.util.HashSet<java.lang.Class<?>> classes = new HashSet<Class<?>>();
     classes.add(TableServiceImpl.class);
-    classes.add(XStreamAliasProvider.class);
+    classes.add(SimpleXMLMessageReaderWriter.class);
     classes.add(ODKDatastoreExceptionMapper.class);
     classes.add(ODKTablesExceptionMapper.class);
     classes.add(RuntimeExceptionMapper.class);
