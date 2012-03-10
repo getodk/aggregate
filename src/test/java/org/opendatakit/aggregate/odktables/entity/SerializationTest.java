edccc3f83750771781179e4ee3cdaa6688535a02
the dylan price <the.dylan.price@gmail.com>
2012-03-09 20:26:13 -0800

    Updated SerializationTest to test simple xml serialization.

--- src/test/java/org/opendatakit/aggregate/odktables/entity/SerializationTest.java
+++ src/test/java/org/opendatakit/aggregate/odktables/entity/SerializationTest.java
@@ -2,6 +2,8 @@ package org.opendatakit.aggregate.odktables.entity;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,89 +14,110 @@ import org.opendatakit.aggregate.odktables.entity.Column.ColumnType;
 import org.opendatakit.aggregate.odktables.entity.api.RowResource;
 import org.opendatakit.aggregate.odktables.entity.api.TableDefinition;
 import org.opendatakit.aggregate.odktables.entity.api.TableResource;
-
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.StaxDriver;
+import org.opendatakit.aggregate.odktables.entity.serialization.ListConverter;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.convert.Registry;
+import org.simpleframework.xml.convert.RegistryStrategy;
+import org.simpleframework.xml.core.Persister;
+import org.simpleframework.xml.strategy.Strategy;
 
 public class SerializationTest {
 
-  private XStream xstream;
+  private Serializer serializer;
+  private StringWriter writer;
 
   @Before
   public void setUp() throws Exception {
-    this.xstream = new XStream(new StaxDriver());
-    this.xstream.processAnnotations(new Class[] { Column.class, Row.class, TableEntry.class,
-        RowResource.class, TableDefinition.class, TableResource.class });
+    Registry registry = new Registry();
+    Strategy strategy = new RegistryStrategy(registry);
+    serializer = new Persister(strategy);
+    ListConverter converter = new ListConverter(serializer);
+    registry.bind(ArrayList.class, converter);
+    
+    writer = new StringWriter();
   }
 
   @Test
-  public void testRow() {
+  public void testRow() throws Exception {
     Row expected = Row.forUpdate("1", "5", T.Data.DYLAN.getValues());
-    String xml = xstream.toXML(expected);
+    serializer.write(expected, writer);
+    String xml = writer.toString();
     System.out.println(xml);
-    Row actual = (Row) xstream.fromXML(xml);
+    Row actual = serializer.read(Row.class, xml);
     assertEquals(expected, actual);
   }
 
   @Test
-  public void testColumn() {
+  public void testColumn() throws Exception {
     Column expected = new Column("name", ColumnType.STRING);
-    String xml = xstream.toXML(expected);
+    serializer.write(expected, writer);
+    String xml = writer.toString();
     System.out.println(xml);
-    Column actual = (Column) xstream.fromXML(xml);
+    Column actual = serializer.read(Column.class, xml);
     assertEquals(expected, actual);
   }
 
   @Test
-  public void testTableEntry() {
+  public void testTableEntry() throws Exception {
     TableEntry expected = new TableEntry("1", "2");
-    String xml = xstream.toXML(expected);
+    serializer.write(expected, writer);
+    String xml = writer.toString();
     System.out.println(xml);
-    TableEntry actual = (TableEntry) xstream.fromXML(xml);
+    TableEntry actual = serializer.read(TableEntry.class, xml);
     assertEquals(expected, actual);
   }
 
   @Test
-  public void testRowResource() {
+  public void testRowResource() throws Exception {
     Map<String, String> values = T.Data.DYLAN.getValues();
     RowResource expected = new RowResource(Row.forInsert("1", null, values));
     expected.setSelfUri("http://localhost:8080/odktables/tables/1/rows/1");
     expected.setTableUri("http://localhost:8080/odktables/tables/1");
 
-    String xml = xstream.toXML(expected);
+    serializer.write(expected, writer);
+    String xml = writer.toString();
     System.out.println(xml);
-    RowResource actual = (RowResource) xstream.fromXML(xml);
+    RowResource actual = serializer.read(RowResource.class, xml);
     assertEquals(expected, actual);
   }
 
   @Test
-  public void testTableDefinition() {
+  public void testTableDefinition() throws Exception {
     TableDefinition expected = new TableDefinition(T.columns);
-    String xml = xstream.toXML(expected);
+    serializer.write(expected, writer);
+    String xml = writer.toString();
     System.out.println(xml);
-    TableDefinition actual = (TableDefinition) xstream.fromXML(xml);
+    TableDefinition actual = serializer.read(TableDefinition.class, xml);
     assertEquals(expected, actual);
   }
 
   @Test
-  public void testTableResource() {
+  public void testTableResource() throws Exception {
     TableEntry entry = new TableEntry("1", "2");
     TableResource expected = new TableResource(entry);
     expected.setSelfUri("http://localhost:8080/odktables/tables/1");
     expected.setDataUri("http://localhost:8080/odktables/tables/1/rows");
     expected.setColumnsUri("http://localhost:8080/odktables/tables/1/columns");
     expected.setDiffUri("http://localhost:8080/odktables/tables/1/rows/diff");
-    String xml = xstream.toXML(expected);
+    serializer.write(expected, writer);
+    String xml = writer.toString();
     System.out.println(xml);
-    TableResource actual = (TableResource) xstream.fromXML(xml);
+    TableResource actual = serializer.read(TableResource.class, xml);
     assertEquals(expected, actual);
   }
-  
+
   @Test
-  public void testDeserialize()
-  {
-    String xml = "<list><org.opendatakit.aggregate.odktables.entity.api.TableResource><tableId>people</tableId><dataEtag>0</dataEtag><selfUri>http://localhost:8888/odktables/tables/people</selfUri><columnsUri>http://localhost:8888/odktables/tables/people/columns</columnsUri><dataUri>http://localhost:8888/odktables/tables/people/rows</dataUri><diffUri>http://localhost:8888/odktables/tables/people/rows/diff</diffUri></org.opendatakit.aggregate.odktables.entity.api.TableResource></list>" ;
-    List<TableResource> resources = (List<TableResource>)xstream.fromXML(xml);
-        
+  public void testListOfRow() throws Exception {
+    List<Row> expected = new ArrayList<Row>();
+    Row one = Row.forInsert("1", null, T.Data.DYLAN.getValues());
+    Row two = Row.forInsert("1", null, T.Data.JOHN.getValues());
+    expected.add(one);
+    expected.add(two);
+
+    serializer.write(expected, writer);
+    String xml = writer.toString();
+    System.out.println(xml);
+    List<Row> actual = (List<Row>) serializer.read(ArrayList.class, xml);
+    assertEquals(expected, actual);
   }
 }
