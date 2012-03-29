45f8377a58228db8df72187a1ec350eefefa5120
Dylan Price <the.dylan.price@gmail.com>
2012-03-28 23:27:31 -0700

    Added tests using RestTemplate.

---
+++ src/it/java/org/opendatakit/aggregate/odktables/api/DataServiceTest.java
@@ -0,0 +1,55 @@
+package org.opendatakit.aggregate.odktables.api;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.net.URI;
+import java.util.List;
+
+import org.junit.Test;
+import org.opendatakit.aggregate.odktables.T;
+import org.opendatakit.aggregate.odktables.api.client.Util;
+import org.opendatakit.aggregate.odktables.entity.Row;
+import org.opendatakit.aggregate.odktables.entity.api.RowResource;
+import org.opendatakit.aggregate.odktables.entity.api.TableResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+public class DataServiceTest extends AbstractServiceTest {
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    super.createTable();
+    TableResource resource = rt.getForObject(baseUri.resolve(T.tableId), TableResource.class);
+    baseUri = URI.create(resource.getDataUri());
+  }
+  
+  @Override
+  public void tearDown() throws Exception {
+    baseUri = baseUri.resolve("/odktables/tables/");
+    super.tearDown();
+  }
+
+  @Test
+  public void testGetRowsNoRows() {
+    List<?> rows = rt.getForObject(baseUri, List.class);
+    assertTrue(rows.isEmpty());
+  }
+
+  @Test
+  public void testInsertRow() {
+    String rowId = T.Data.DYLAN.getId();
+    String uri = Util.buildUri(baseUri.toASCIIString(), rowId);
+
+    Row expected = Row.forInsert(rowId, null, T.Data.DYLAN.getValues());
+    HttpEntity<Row> entity = super.entity(expected);
+
+    ResponseEntity<RowResource> resp = rt.exchange(uri, HttpMethod.PUT, entity, RowResource.class);
+    Row actual = resp.getBody();
+    assertEquals(expected.getRowId(), actual.getRowId());
+    assertNotNull(actual.getRowEtag());
+  }
+
+}
