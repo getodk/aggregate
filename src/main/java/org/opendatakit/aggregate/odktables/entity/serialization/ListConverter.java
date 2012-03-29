9e1816e25e9715fb5f0731dffcb900c20744ee67
Dylan Price <the.dylan.price@gmail.com>
2012-03-28 23:25:48 -0700

    Fixed null pointer bug.

--- src/main/java/org/opendatakit/aggregate/odktables/entity/serialization/ListConverter.java
+++ src/main/java/org/opendatakit/aggregate/odktables/entity/serialization/ListConverter.java
@@ -3,7 +3,6 @@ package org.opendatakit.aggregate.odktables.entity.serialization;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.Validate;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.convert.Converter;
 import org.simpleframework.xml.stream.InputNode;
@@ -20,8 +19,9 @@ public class ListConverter implements Converter<List<?>> {
   @Override
   public List<?> read(InputNode node) throws Exception {
     List<Object> list = new ArrayList<Object>();
-    String type = node.getAttribute("type").getValue();
-    if (type != null) {
+    InputNode typeNode = node.getAttribute("type");
+    if (typeNode != null) {
+      String type = typeNode.getValue();
       Class<?> clazz = Class.forName(type);
       InputNode innerNode;
       while ((innerNode = node.getNext()) != null) {
