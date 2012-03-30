722a4a5bfe411e7adae21c93c2785d24127176b6
Dylan Price <the.dylan.price@gmail.com>
2012-03-30 01:51:41 -0700

    Fixed import.

--- src/it/java/org/opendatakit/aggregate/odktables/api/DataServiceTest.java
+++ src/it/java/org/opendatakit/aggregate/odktables/api/DataServiceTest.java
@@ -7,7 +7,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.junit.Test;
-import org.opendatakit.aggregate.odktables.T;
+import org.opendatakit.aggregate.odktables.api.client.T;
 import org.opendatakit.aggregate.odktables.api.client.Util;
 import org.opendatakit.aggregate.odktables.entity.Row;
 import org.opendatakit.aggregate.odktables.entity.api.RowResource;
