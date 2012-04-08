eb5900cf73fd373c4fb484e11dac02db123f904b
Dylan Price <the.dylan.price@gmail.com>
2012-04-07 19:45:18 -0700

    Updated imports.

--- src/it/java/org/opendatakit/aggregate/odktables/api/DataServiceTest.java
+++ src/it/java/org/opendatakit/aggregate/odktables/api/DataServiceTest.java
@@ -1,6 +1,7 @@
 package org.opendatakit.aggregate.odktables.api;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.net.URI;
