bc8882e972196a551d06571e837f00765bae45ee
Dylan Price <the.dylan.price@gmail.com>
2012-05-09 12:39:37 -0700

    Added methods to convert between lists of TablePermission and datastore representation.

--- src/main/java/org/opendatakit/aggregate/odktables/relation/RUtil.java
+++ src/main/java/org/opendatakit/aggregate/odktables/relation/RUtil.java
@@ -1,5 +1,11 @@
 package org.opendatakit.aggregate.odktables.relation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.opendatakit.aggregate.odktables.entity.TablePermission;
+
 /**
  * @author the.dylan.price@gmail.com
  * 
@@ -12,7 +18,29 @@ public class RUtil {
    * Converts a uuid to a datastore table and column name friendly format.
    */
   public static String convertIdentifier(String id) {
-	id = "table:" + id;
+    id = "table:" + id;
     return id.replace('-', '_').replace(':', '_').toUpperCase();
+  }
+
+  /**
+   * Convert a list of {@link TablePermission} into a string as it should be
+   * stored in the datastore
+   */
+  public static String toPermissionsString(List<TablePermission> permissions) {
+    return StringUtils.join(permissions, ',');
+  }
+
+  /**
+   * Convert a string of permissions coming from the datastore to a list of
+   * {@link TablePermission}
+   */
+  public static List<TablePermission> toPermissionsList(String permissions) {
+    String[] strings = StringUtils.split(permissions, ',');
+    List<TablePermission> perms = new ArrayList<TablePermission>();
+    for (String string : strings) {
+      TablePermission permission = TablePermission.valueOf(string);
+      perms.add(permission);
+    }
+    return perms;
   }
 }
