b1f6d6591b65af4b715d77513c165d064d8238c0
Dylan Price <the.dylan.price@gmail.com>
2012-05-13 17:25:45 -0700

    Added tests of getRows and getRowsSince by Scope.

--- src/test/java/org/opendatakit/aggregate/odktables/DataManagerTest.java
+++ src/test/java/org/opendatakit/aggregate/odktables/DataManagerTest.java
@@ -14,7 +14,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.opendatakit.aggregate.odktables.entity.Row;
+import org.opendatakit.aggregate.odktables.entity.Scope;
+import org.opendatakit.aggregate.odktables.entity.Scope.Type;
 import org.opendatakit.aggregate.odktables.entity.TableEntry;
+import org.opendatakit.aggregate.odktables.exception.BadColumnNameException;
 import org.opendatakit.aggregate.odktables.exception.EtagMismatchException;
 import org.opendatakit.common.persistence.exception.ODKDatastoreException;
 import org.opendatakit.common.persistence.exception.ODKEntityNotFoundException;
@@ -22,6 +25,9 @@ import org.opendatakit.common.persistence.exception.ODKEntityPersistException;
 import org.opendatakit.common.persistence.exception.ODKTaskLockException;
 import org.opendatakit.common.web.CallingContext;
 import org.opendatakit.common.web.TestContextFactory;
+
+import com.google.common.collect.Maps;
+import com.google.gwt.thirdparty.guava.common.collect.Lists;
 
 public class DataManagerTest {
 
@@ -65,7 +71,7 @@ public class DataManagerTest {
 
   @Test
   public void testInsertRows() throws ODKEntityPersistException, ODKDatastoreException,
-      ODKTaskLockException {
+      ODKTaskLockException, BadColumnNameException {
     List<Row> actualRows = dm.insertRows(rows);
     assertEquals(2, actualRows.size());
     for (int i = 0; i < rows.size(); i++) {
@@ -81,13 +87,14 @@ public class DataManagerTest {
 
   @Test(expected = ODKEntityPersistException.class)
   public void testInsertRowsAlreadyExist() throws ODKEntityPersistException, ODKDatastoreException,
-      ODKTaskLockException {
+      ODKTaskLockException, BadColumnNameException {
     dm.insertRows(rows);
     dm.insertRows(rows);
   }
 
   @Test
-  public void testGetRows() throws ODKDatastoreException, ODKTaskLockException {
+  public void testGetRows() throws ODKDatastoreException, ODKTaskLockException,
+      BadColumnNameException {
     dm.insertRows(rows);
     List<Row> actualRows = dm.getRows();
     for (int i = 0; i < rows.size(); i++) {
@@ -99,7 +106,30 @@ public class DataManagerTest {
   }
 
   @Test
-  public void testGetRow() throws ODKDatastoreException, ODKTaskLockException {
+  public void testGetRowsByScope() throws ODKEntityPersistException, ODKDatastoreException,
+      ODKTaskLockException, EtagMismatchException, BadColumnNameException {
+    List<Row> rows = setupTestRows();
+    Row expected = rows.get(0);
+    List<Row> actualRows = dm.getRows(expected.getFilterScope());
+    assertEquals(1, actualRows.size());
+    Row actual = actualRows.get(0);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testGetRowsByScopes() throws ODKEntityPersistException, EtagMismatchException,
+      BadColumnNameException, ODKDatastoreException, ODKTaskLockException {
+    List<Row> rows = setupTestRows();
+    Row row1 = rows.get(0);
+    Row row2 = rows.get(1);
+    List<Scope> scopes = Lists.newArrayList(row1.getFilterScope(), row2.getFilterScope());
+    List<Row> results = dm.getRows(scopes);
+    assertEquals(2, results.size());
+  }
+
+  @Test
+  public void testGetRow() throws ODKDatastoreException, ODKTaskLockException,
+      BadColumnNameException {
     Row expected = Row.forInsert(T.Data.DYLAN.getId(), T.Data.DYLAN.getValues());
     expected = dm.insertRow(expected);
     Row actual = dm.getRow(T.Data.DYLAN.getId());
@@ -114,7 +144,7 @@ public class DataManagerTest {
 
   @Test
   public void testGetRowNullSafe() throws ODKEntityPersistException, ODKDatastoreException,
-      ODKTaskLockException {
+      ODKTaskLockException, BadColumnNameException {
     Row expected = Row.forInsert(T.Data.DYLAN.getId(), T.Data.DYLAN.getValues());
     expected = dm.insertRow(expected);
     Row actual = dm.getRowNullSafe(T.Data.DYLAN.getId());
@@ -129,7 +159,7 @@ public class DataManagerTest {
 
   @Test
   public void testUpdateRow() throws ODKEntityPersistException, ODKDatastoreException,
-      ODKTaskLockException, EtagMismatchException {
+      ODKTaskLockException, EtagMismatchException, BadColumnNameException {
     rows = dm.insertRows(rows);
     Row expected = rows.get(0);
     expected.getValues().put(T.Columns.age, "24");
@@ -141,7 +171,7 @@ public class DataManagerTest {
 
   @Test(expected = EtagMismatchException.class)
   public void testUpdateRowVersionMismatch() throws ODKEntityPersistException,
-      ODKDatastoreException, ODKTaskLockException, EtagMismatchException {
+      ODKDatastoreException, ODKTaskLockException, EtagMismatchException, BadColumnNameException {
     rows = dm.insertRows(rows);
     Row row = rows.get(0);
     row.setRowEtag(UUID.randomUUID().toString());
@@ -150,15 +180,53 @@ public class DataManagerTest {
 
   @Test(expected = ODKEntityNotFoundException.class)
   public void testUpdateRowDoesNotExist() throws ODKEntityNotFoundException, ODKDatastoreException,
-      ODKTaskLockException, EtagMismatchException {
+      ODKTaskLockException, EtagMismatchException, BadColumnNameException {
     Row row = rows.get(0);
     row.setRowEtag(UUID.randomUUID().toString());
     dm.updateRow(row);
   }
 
   @Test
+  public void testUpdateRowDoesNotChangeFilter() throws ODKEntityPersistException,
+      ODKDatastoreException, ODKTaskLockException, EtagMismatchException, BadColumnNameException {
+    Row expected = rows.get(0);
+    expected.setFilterScope(new Scope(Type.USER, T.user));
+    expected = dm.insertRow(expected);
+    Row actual = Row.forUpdate(expected.getRowId(), expected.getRowEtag(),
+        Maps.<String, String> newHashMap());
+    actual = dm.updateRow(actual);
+    expected.setRowEtag(actual.getRowEtag());
+    assertEquals(expected.getFilterScope(), actual.getFilterScope());
+  }
+
+  @Test
+  public void testUpdateRowDoesChangeFilter() throws ODKEntityNotFoundException,
+      EtagMismatchException, ODKDatastoreException, ODKTaskLockException, BadColumnNameException {
+    Row row = rows.get(0);
+    row.setFilterScope(new Scope(Type.USER, T.user));
+    row = dm.insertRow(row);
+    Row actual = Row
+        .forUpdate(row.getRowId(), row.getRowEtag(), Maps.<String, String> newHashMap());
+    actual.setFilterScope(Scope.EMPTY_SCOPE);
+    actual = dm.updateRow(actual);
+    row.setRowEtag(actual.getRowEtag());
+    assertEquals(Scope.EMPTY_SCOPE, actual.getFilterScope());
+  }
+
+  @Test(expected = BadColumnNameException.class)
+  public void testUpdateRowBadColumnName() throws ODKEntityPersistException,
+      BadColumnNameException, ODKDatastoreException, ODKTaskLockException, EtagMismatchException {
+    Row row = rows.get(0);
+    row = dm.insertRow(row);
+    Map<String, String> values = Maps.newHashMap();
+    values.put(T.Columns.name + "diff", "value");
+    row = Row.forUpdate(row.getRowId(), row.getRowEtag(), values);
+    dm.updateRow(row);
+  }
+
+  @Test
   public void testDeleteRows() throws ODKEntityPersistException, ODKDatastoreException,
-      ODKTaskLockException {
+      ODKTaskLockException, BadColumnNameException {
     dm.insertRows(rows);
     dm.deleteRows(Util.list(T.Data.DYLAN.getId(), T.Data.JOHN.getId()));
     List<Row> rows = dm.getRows();
@@ -167,7 +235,7 @@ public class DataManagerTest {
 
   @Test
   public void testGetRowsSince() throws ODKEntityPersistException, ODKDatastoreException,
-      ODKTaskLockException, EtagMismatchException {
+      ODKTaskLockException, EtagMismatchException, BadColumnNameException {
     TableEntry entry = tm.getTableNullSafe(tableId);
     String beginEtag = entry.getDataEtag();
     rows = dm.insertRows(rows);
@@ -192,6 +260,65 @@ public class DataManagerTest {
 
     List<Row> actual = dm.getRowsSince(beginEtag);
     Util.assertCollectionSameElements(expected.values(), actual);
+  }
+
+  @Test
+  public void testGetRowsSinceByScope() throws ODKEntityPersistException, EtagMismatchException,
+      BadColumnNameException, ODKDatastoreException, ODKTaskLockException {
+    TableEntry entry = tm.getTableNullSafe(tableId);
+    String beginEtag = entry.getDataEtag();
+    List<Row> rows = setupTestRows();
+    Scope scope = rows.get(0).getFilterScope();
+    List<Row> expected = dm.getRows(scope);
+    List<Row> actual = dm.getRowsSince(beginEtag, scope);
+    Util.assertCollectionSameElements(expected, actual);
+  }
+
+  @Test
+  public void testGetRowsSinceByScopes() throws ODKDatastoreException, EtagMismatchException,
+      BadColumnNameException, ODKTaskLockException {
+    TableEntry entry = tm.getTableNullSafe(tableId);
+    String beginEtag = entry.getDataEtag();
+    List<Row> rows = setupTestRows();
+    Row row2 = rows.get(1);
+    Row row3 = rows.get(2);
+    List<Scope> scopes = Lists.newArrayList(row2.getFilterScope(), row3.getFilterScope());
+    List<Row> expected = dm.getRows(scopes);
+    List<Row> actual = dm.getRowsSince(beginEtag, scopes);
+    Util.assertCollectionSameElements(expected, actual);
+  }
+
+  private List<Row> setupTestRows() throws ODKEntityPersistException, ODKDatastoreException,
+      ODKTaskLockException, EtagMismatchException, BadColumnNameException {
+    Map<String, String> values = Maps.newHashMap();
+    List<Row> rows = Lists.newArrayList();
+
+    Row row = Row.forInsert("1", values);
+    row.setFilterScope(new Scope(Type.DEFAULT, null));
+    rows.add(row);
+
+    row = Row.forInsert("2", values);
+    row.setFilterScope(new Scope(Type.USER, T.user));
+    rows.add(row);
+
+    row = Row.forInsert("3", values);
+    row.setFilterScope(new Scope(Type.GROUP, T.group));
+    rows.add(row);
+
+    row = Row.forInsert("4", values);
+    row.setFilterScope(Scope.EMPTY_SCOPE);
+    rows.add(row);
+
+    rows = dm.insertRows(rows);
+    values.put(T.Columns.name, "some name");
+
+    for (Row update : rows) {
+      update.setValues(values);
+      update.setFilterScope(null);
+    }
+    rows = dm.updateRows(rows);
+
+    return rows;
   }
 
 }
