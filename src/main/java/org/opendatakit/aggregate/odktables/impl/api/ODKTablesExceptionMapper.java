365a11d1dfbb92c8da36fe763c0cb9040e99805b
Dylan Price <the.dylan.price@gmail.com>
2012-05-14 00:44:21 -0700

    Changed all output types of exceptions to text/xml.

--- src/main/java/org/opendatakit/aggregate/odktables/impl/api/ODKTablesExceptionMapper.java
+++ src/main/java/org/opendatakit/aggregate/odktables/impl/api/ODKTablesExceptionMapper.java
@@ -1,5 +1,6 @@
 package org.opendatakit.aggregate.odktables.impl.api;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -20,19 +21,24 @@ public class ODKTablesExceptionMapper implements ExceptionMapper<ODKTablesExcept
   public Response toResponse(ODKTablesException e) {
     if (e instanceof EtagMismatchException) {
       return Response.status(Status.PRECONDITION_FAILED)
-          .entity(new Error(ErrorType.ETAG_MISMATCH, e.getMessage())).build();
+          .entity(new Error(ErrorType.ETAG_MISMATCH, e.getMessage())).type(MediaType.TEXT_XML)
+          .build();
     } else if (e instanceof TableAlreadyExistsException) {
       return Response.status(Status.CONFLICT)
-          .entity(new Error(ErrorType.TABLE_EXISTS, e.getMessage())).build();
+          .entity(new Error(ErrorType.TABLE_EXISTS, e.getMessage())).type(MediaType.TEXT_XML)
+          .build();
     } else if (e instanceof PermissionDeniedException) {
       return Response.status(Status.FORBIDDEN)
-          .entity(new Error(ErrorType.PERMISSION_DENIED, e.getMessage())).build();
+          .entity(new Error(ErrorType.PERMISSION_DENIED, e.getMessage())).type(MediaType.TEXT_XML)
+          .build();
     } else if (e instanceof BadColumnNameException) {
       return Response.status(Status.BAD_REQUEST)
-          .entity(new Error(ErrorType.BAD_COLUMN_NAME, e.getMessage())).build();
+          .entity(new Error(ErrorType.BAD_COLUMN_NAME, e.getMessage())).type(MediaType.TEXT_XML)
+          .build();
     } else {
       return Response.status(Status.BAD_REQUEST)
-          .entity(new Error(ErrorType.BAD_REQUEST, e.getMessage())).build();
+          .entity(new Error(ErrorType.BAD_REQUEST, e.getMessage())).type(MediaType.TEXT_XML)
+          .build();
     }
   }
 
