da043ff3fb42273eb5342800e76b1262c2e240e8
mitchellsundt <mitchellsundt@gmail.com>
2013-05-19 23:10:04 -0700

    Minor reformatting, update license file, and remove unused jar.

--- src/main/java/org/opendatakit/aggregate/odktables/entity/OdkTablesFileManifestEntry.java
+++ src/main/java/org/opendatakit/aggregate/odktables/entity/OdkTablesFileManifestEntry.java
@@ -1,32 +1,31 @@
 package org.opendatakit.aggregate.odktables.entity;
 
 /**
- * This represents information about a file so that a phone
- * running ODKTables will be able to check to see if it has
- * the most recent version of the file, and if not will be 
- * able to download the file. It is meant to be mostly 
- * a struct that is parsed into and recovered from JSON,
- * and work in tandem with {@link OdkTablesKeyValueStoreEntry}.
+ * This represents information about a file so that a phone running ODKTables
+ * will be able to check to see if it has the most recent version of the file,
+ * and if not will be able to download the file. It is meant to be mostly a
+ * struct that is parsed into and recovered from JSON, and work in tandem with
+ * {@link OdkTablesKeyValueStoreEntry}.
+ *
  * @author sudar.sam@gmail.com
  *
  */
 public class OdkTablesFileManifestEntry {
-	
-	/**
-	 * This is the name of the file.
-	 */
-	public String filename;
-	
-	/**
-	 * This is the md5hash of the file, which will be
-	 * used for checking whether or not the version of the file 
-	 * on the phone is current.
-	 */
-	public String md5hash;
-	
-	/**
-	 * This is the url from which the current version of the
-	 * file can be downloaded.
-	 */
-	public String downloadUrl;
+
+  /**
+   * This is the name of the file.
+   */
+  public String filename;
+
+  /**
+   * This is the md5hash of the file, which will be used for checking whether or
+   * not the version of the file on the phone is current.
+   */
+  public String md5hash;
+
+  /**
+   * This is the url from which the current version of the file can be
+   * downloaded.
+   */
+  public String downloadUrl;
 }
