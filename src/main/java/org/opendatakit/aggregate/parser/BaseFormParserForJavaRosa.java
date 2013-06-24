1a0536bcf5831021789f9ab27b8fcbd559cac37e
mitchellsundt <mitchellsundt@gmail.com>
2013-06-24 12:01:53 -0700

    issue 854 - Add missing initialization that could impact parsing of form (unclear whether it does or not).

--- src/main/java/org/opendatakit/aggregate/parser/BaseFormParserForJavaRosa.java
+++ src/main/java/org/opendatakit/aggregate/parser/BaseFormParserForJavaRosa.java
@@ -36,6 +36,8 @@ import org.javarosa.core.model.SubmissionProfile;
 import org.javarosa.core.model.instance.FormInstance;
 import org.javarosa.core.model.instance.TreeElement;
 import org.javarosa.core.model.instance.TreeReference;
+import org.javarosa.core.services.PrototypeManager;
+import org.javarosa.model.xform.XFormsModule;
 import org.javarosa.xform.parse.XFormParser;
 import org.javarosa.xform.util.XFormUtils;
 import org.kxml2.kdom.Document;
@@ -59,6 +61,57 @@ public class BaseFormParserForJavaRosa {
 
   private static final String LEADING_QUESTION_XML_PATTERN = "^[^<]*<\\s*\\?\\s*xml.*";
   private static final Log log = LogFactory.getLog(BaseFormParserForJavaRosa.class.getName());
+
+  /**
+   * Classes needed to serialize objects. Need to put anything from JR in here.
+   */
+  public final static String[] SERIALIABLE_CLASSES = {
+       "org.javarosa.core.services.locale.ResourceFileDataSource", // JavaRosaCoreModule
+       "org.javarosa.core.services.locale.TableLocaleSource", // JavaRosaCoreModule
+          "org.javarosa.core.model.FormDef",
+       "org.javarosa.core.model.SubmissionProfile", // CoreModelModule
+       "org.javarosa.core.model.QuestionDef", // CoreModelModule
+       "org.javarosa.core.model.GroupDef", // CoreModelModule
+       "org.javarosa.core.model.instance.FormInstance", // CoreModelModule
+       "org.javarosa.core.model.data.BooleanData", // CoreModelModule
+       "org.javarosa.core.model.data.DateData", // CoreModelModule
+       "org.javarosa.core.model.data.DateTimeData", // CoreModelModule
+       "org.javarosa.core.model.data.DecimalData", // CoreModelModule
+       "org.javarosa.core.model.data.GeoPointData", // CoreModelModule
+       "org.javarosa.core.model.data.IntegerData", // CoreModelModule
+       "org.javarosa.core.model.data.LongData", // CoreModelModule
+       "org.javarosa.core.model.data.MultiPointerAnswerData", // CoreModelModule
+       "org.javarosa.core.model.data.PointerAnswerData", // CoreModelModule
+       "org.javarosa.core.model.data.SelectMultiData", // CoreModelModule
+       "org.javarosa.core.model.data.SelectOneData", // CoreModelModule
+       "org.javarosa.core.model.data.StringData", // CoreModelModule
+       "org.javarosa.core.model.data.TimeData", // CoreModelModule
+       "org.javarosa.core.model.data.UncastData", // CoreModelModule
+       "org.javarosa.core.model.data.helper.BasicDataPointer" // CoreModelModule
+  };
+
+  private static boolean isJavaRosaInitialized = false;
+  /**
+   * The JR implementation here does not look thread-safe or
+   * like something to be invoked more than once.
+   * Moving it within a critical section and a do-once guard.
+   */
+  private static final void initializeJavaRosa() {
+    synchronized (log) {
+       if ( !isJavaRosaInitialized ) {
+             // need a list of classes that formdef uses
+             // unfortunately, the JR registerModule() functions do more than this.
+             // register just the classes that would have been registered by:
+             // new JavaRosaCoreModule().registerModule();
+             // new CoreModelModule().registerModule();
+             // replace with direct call to PrototypeManager
+             PrototypeManager.registerPrototypes(SERIALIABLE_CLASSES);
+             new XFormsModule().registerModule();
+             isJavaRosaInitialized = true;
+       }
+    }
+  }
+
   private static final String BASE64_ENCRYPTED_FIELD_KEY = "base64EncryptedFieldKey";
   private static final String BASE64_RSA_PUBLIC_KEY = "base64RsaPublicKey";
 
@@ -445,6 +498,8 @@ public class BaseFormParserForJavaRosa {
     }
 
     xml = existingXml;
+
+    initializeJavaRosa();
 
     XFormParserWithBindEnhancements xfp = parseFormDefinition(xml, this);
     try {
