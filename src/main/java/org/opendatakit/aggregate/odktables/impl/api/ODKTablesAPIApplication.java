deaedd1647d2947ae270a6f15abb90b691550b01
Sam Sudar <sudar.sam@gmail.com>
2013-07-19 17:41:15 -0700

    can put a file up to tables and get it back down, but includes all the headers and has the wrong content type.

--- src/main/java/org/opendatakit/aggregate/odktables/impl/api/ODKTablesAPIApplication.java
+++ src/main/java/org/opendatakit/aggregate/odktables/impl/api/ODKTablesAPIApplication.java
@@ -29,6 +29,7 @@ public class ODKTablesAPIApplication extends Application {
   public Set<Class<?>> getClasses() {
     final java.util.HashSet<java.lang.Class<?>> classes = new HashSet<Class<?>>();
     classes.add(TableServiceImpl.class);
+    classes.add(FileServiceImpl.class);
     classes.add(SimpleXMLMessageReaderWriter.class);
     classes.add(ODKDatastoreExceptionMapper.class);
     classes.add(ODKTablesExceptionMapper.class);
