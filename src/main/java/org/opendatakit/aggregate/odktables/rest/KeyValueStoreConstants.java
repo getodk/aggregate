d35a358566db4fe2d474be3684dd8e96050b9c4a
mitchellsundt <mitchellsundt@gmail.com>
2013-10-21 17:05:06 -0700

    Update names, properties to conform to those in Tables

--- src/main/java/org/opendatakit/aggregate/odktables/rest/KeyValueStoreConstants.java
+++ src/main/java/org/opendatakit/aggregate/odktables/rest/KeyValueStoreConstants.java
@@ -10,26 +10,25 @@ public class KeyValueStoreConstants {
 
 
   // special well-known partitions
-  public static final String TABLE_PARTITION = "Table";
-  public static final String COLUMN_PARTITION = "Column";
+  public static final String PARTITION_TABLE = "Table";
+  public static final String PARTITION_COLUMN = "Column";
   // others should use their class names as the partition name
 
   // default aspect
-  public static final String DEFAULT_ASPECT = "default";
+  public static final String ASPECT_DEFAULT = "default";
 
   // Keys for:
   // TABLE_PARTITION, DEFAULT_ASPECT...
   public static final String TABLE_TYPE = "tableType";
   public static final String TABLE_ACCESS_CONTROL_TABLE_ID = "accessControlTableId";
-
   public static final String TABLE_DISPLAY_NAME = "displayName";
+  public static final String TABLE_COL_ORDER = "colOrder";
   public static final String TABLE_PRIME_COLS = "primeCols";
   public static final String TABLE_SORT_COL = "sortCol";
-  public static final String TABLE_CO_VIEW_SETTINGS = "coViewSettings";
-  public static final String TABLE_DETAIL_VIEW_FILE = "detailViewFile";
+  public static final String TABLE_INDEX_COL = "indexCol";
+  public static final String TABLE_CURRENT_VIEW_TYPE = "currentViewType";
   public static final String TABLE_SUMMARY_DISPLAY_FORMAT = "summaryDisplayFormat";
-  public static final String TABLE_COL_ORDER = "colOrder";
-  public static final String TABLE_OV_VIEW_SETTINGS = "ovViewSettings";
+  public static final String TABLE_CURRENT_QUERY = "currentQuery";
 
   // Keys for:
   // COLUMN_PARTITION, dbColName...
