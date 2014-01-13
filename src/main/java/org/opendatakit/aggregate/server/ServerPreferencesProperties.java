6d6f9f04c3ff2baa5c3f351470c5523cfffa3b54
mitchellsundt <mitchellsundt@gmail.com>
2014-01-13 12:47:09 -0800

    Initial check-in Enketo integration from SDRC India

--- src/main/java/org/opendatakit/aggregate/server/ServerPreferencesProperties.java
+++ src/main/java/org/opendatakit/aggregate/server/ServerPreferencesProperties.java
@@ -36,11 +36,10 @@ public class ServerPreferencesProperties extends CommonFieldsBase {
 
   private static final String TABLE_NAME = "_server_preferences_properties";
 
-  private static final DataField KEY = new DataField("KEY",
-      DataField.DataType.STRING, true, 128L);
+  private static final DataField KEY = new DataField("KEY", DataField.DataType.STRING, true, 128L);
 
-  private static final DataField VALUE = new DataField("VALUE",
-      DataField.DataType.STRING, true, 20480L);
+  private static final DataField VALUE = new DataField("VALUE", DataField.DataType.STRING, true,
+      20480L);
 
   // these values are set in the ServiceAccountPrivateKeyUploadServlet
   // and used everywhere else when requesting access
@@ -48,7 +47,14 @@ public class ServerPreferencesProperties extends CommonFieldsBase {
   public static final String GOOGLE_API_SERVICE_ACCOUNT_EMAIL = "GOOGLE_SERVICE_ACCOUNT_EMAIL";
   public static final String PRIVATE_KEY_FILE_CONTENTS = "PRIVATE_KEY_FILE_CONTENTS";
 
-  private static final String GOOGLE_SIMPLE_API_KEY = "GOOG_SIMPLE_API_KEY"; // supplied to Google Maps only
+  private static final String GOOGLE_SIMPLE_API_KEY = "GOOG_SIMPLE_API_KEY"; // supplied
+                                                                             // to
+                                                                             // Google
+                                                                             // Maps
+                                                                             // only
+
+  private static final String ENKETO_API_URL = "ENKETO_API_URL";
+  private static final String ENKETO_API_TOKEN = "ENKETO_API_TOKEN";
 
   // other keys...
   private static final String SITE_KEY = "SITE_KEY";
@@ -65,8 +71,10 @@ public class ServerPreferencesProperties extends CommonFieldsBase {
 
   private static final String ODK_TABLES_SEQUENCER_BASE = "ODK_TABLES_SEQUENCER_BASE";
   // there can be only one APP_ID per ODK Aggregate. Store the app name here.
-  // The main impact on this is validity checking on sync when the appId is checked.
+  // The main impact on this is validity checking on sync when the appId is
+  // checked.
   private static final String ODK_TABLES_APP_ID = "ODK_TABLES_APP_ID";
+
   /**
    * Construct a relation prototype.
    *
@@ -79,8 +87,11 @@ public class ServerPreferencesProperties extends CommonFieldsBase {
     fieldList.add(VALUE);
   }
 
-  public static PreferenceSummary getPreferenceSummary(CallingContext cc) throws ODKEntityNotFoundException, ODKOverQuotaException {
-    return new PreferenceSummary(getGoogleSimpleApiKey(cc), getGoogleApiClientId(cc), getOdkTablesEnabled(cc), getFasterBackgroundActionsDisabled(cc));
+  public static PreferenceSummary getPreferenceSummary(CallingContext cc)
+      throws ODKEntityNotFoundException, ODKOverQuotaException {
+    return new PreferenceSummary(getGoogleSimpleApiKey(cc), getGoogleApiClientId(cc),
+        getEnketoApiUrl(cc), getEnketoApiToken(cc), getOdkTablesEnabled(cc),
+        getFasterBackgroundActionsDisabled(cc));
   }
 
   /**
@@ -98,9 +109,10 @@ public class ServerPreferencesProperties extends CommonFieldsBase {
     return new ServerPreferencesProperties(this, user);
   }
 
-  public static String getSiteKey(CallingContext cc) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static String getSiteKey(CallingContext cc) throws ODKEntityNotFoundException,
+      ODKOverQuotaException {
     String value = getServerPreferencesProperty(cc, SITE_KEY);
-    if ( value == null ) {
+    if (value == null) {
       // synthesize a new one...
       value = CommonFieldsBase.newUri();
       setServerPreferencesProperty(cc, SITE_KEY, value);
@@ -108,82 +120,117 @@ public class ServerPreferencesProperties extends CommonFieldsBase {
     return value;
   }
 
-  public static void setSiteKey(CallingContext cc, String siteKey) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static void setSiteKey(CallingContext cc, String siteKey)
+      throws ODKEntityNotFoundException, ODKOverQuotaException {
     setServerPreferencesProperty(cc, SITE_KEY, siteKey);
   }
 
-  public static String getGoogleSimpleApiKey(CallingContext cc) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static String getGoogleSimpleApiKey(CallingContext cc) throws ODKEntityNotFoundException,
+      ODKOverQuotaException {
     String value = getServerPreferencesProperty(cc, GOOGLE_SIMPLE_API_KEY);
     return value;
   }
 
-  public static void setGoogleSimpleApiKey(CallingContext cc, String googleSimpleApiKey) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static void setGoogleSimpleApiKey(CallingContext cc, String googleSimpleApiKey)
+      throws ODKEntityNotFoundException, ODKOverQuotaException {
     setServerPreferencesProperty(cc, GOOGLE_SIMPLE_API_KEY, googleSimpleApiKey);
   }
 
-  public static String getGoogleApiClientId(CallingContext cc) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static String getGoogleApiClientId(CallingContext cc) throws ODKEntityNotFoundException,
+      ODKOverQuotaException {
     String value = getServerPreferencesProperty(cc, GOOGLE_API_CLIENT_ID);
     return value;
   }
 
-  public static Boolean getOdkTablesEnabled(CallingContext cc) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static String getEnketoApiUrl(CallingContext cc) throws ODKEntityNotFoundException,
+      ODKOverQuotaException {
+    String value = getServerPreferencesProperty(cc, ENKETO_API_URL);
+    return value;
+  }
+
+  public static void setEnketoApiUrl(CallingContext cc, String enketoApiUrl)
+      throws ODKEntityNotFoundException, ODKOverQuotaException {
+    setServerPreferencesProperty(cc, ENKETO_API_URL, enketoApiUrl);
+  }
+
+  public static String getEnketoApiToken(CallingContext cc) throws ODKEntityNotFoundException,
+      ODKOverQuotaException {
+    String value = getServerPreferencesProperty(cc, ENKETO_API_TOKEN);
+    return value;
+  }
+
+  public static void setEnketoApiToken(CallingContext cc, String enketoApiToken)
+      throws ODKEntityNotFoundException, ODKOverQuotaException {
+    setServerPreferencesProperty(cc, ENKETO_API_TOKEN, enketoApiToken);
+  }
+
+  public static Boolean getOdkTablesEnabled(CallingContext cc) throws ODKEntityNotFoundException,
+      ODKOverQuotaException {
     String value = getServerPreferencesProperty(cc, ODK_TABLES_ENABLED);
-    if ( value != null ) {
+    if (value != null) {
       return Boolean.valueOf(value);
     }
     // null value should be treated as false
     return false;
   }
 
-  public static void setOdkTablesEnabled(CallingContext cc, Boolean enabled) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static void setOdkTablesEnabled(CallingContext cc, Boolean enabled)
+      throws ODKEntityNotFoundException, ODKOverQuotaException {
     setServerPreferencesProperty(cc, ODK_TABLES_ENABLED, enabled.toString());
   }
 
-  public static String unsafeIncOdkTablesSequencerBase(CallingContext cc) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static String unsafeIncOdkTablesSequencerBase(CallingContext cc)
+      throws ODKEntityNotFoundException, ODKOverQuotaException {
     String value = getServerPreferencesProperty(cc, ODK_TABLES_SEQUENCER_BASE);
     String newValue = WebUtils.iso8601Date(new Date());
-    if ( value != null && value.compareTo(newValue) >= 0 ) {
+    if (value != null && value.compareTo(newValue) >= 0) {
       // the saved value String-compares greater
       // than the current time string.
 
       // parse the current time string...
       Date d = WebUtils.parseDate(value);
-      if ( d == null ) {
-        throw new IllegalStateException("The saved ODK_TABLES_SEQUENCER_BASE value could not be parsed!");
+      if (d == null) {
+        throw new IllegalStateException(
+            "The saved ODK_TABLES_SEQUENCER_BASE value could not be parsed!");
       }
       // add 1 millisecond and retry...
-      newValue = WebUtils.iso8601Date(new Date(d.getTime()+1));
-      if ( value.compareTo(newValue) >= 0 ) {
-        throw new IllegalStateException("The new ODK_TABLES_SEQUENCER_BASE value was not greater than the saved value.");
+      newValue = WebUtils.iso8601Date(new Date(d.getTime() + 1));
+      if (value.compareTo(newValue) >= 0) {
+        throw new IllegalStateException(
+            "The new ODK_TABLES_SEQUENCER_BASE value was not greater than the saved value.");
       }
     }
     setServerPreferencesProperty(cc, ODK_TABLES_SEQUENCER_BASE, newValue);
     return newValue;
   }
 
-  public static Boolean getFasterWatchdogCycleEnabled(CallingContext cc) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static Boolean getFasterWatchdogCycleEnabled(CallingContext cc)
+      throws ODKEntityNotFoundException, ODKOverQuotaException {
     String value = getServerPreferencesProperty(cc, FASTER_WATCHDOG_CYCLE_ENABLED);
-    if ( value != null ) {
+    if (value != null) {
       return Boolean.valueOf(value);
     }
     // null value should be treated as false
     return false;
   }
 
-  public static void setFasterWatchdogCycleEnabled(CallingContext cc, Boolean enabled) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static void setFasterWatchdogCycleEnabled(CallingContext cc, Boolean enabled)
+      throws ODKEntityNotFoundException, ODKOverQuotaException {
     setServerPreferencesProperty(cc, FASTER_WATCHDOG_CYCLE_ENABLED, enabled.toString());
   }
 
-  public static Boolean getFasterBackgroundActionsDisabled(CallingContext cc) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static Boolean getFasterBackgroundActionsDisabled(CallingContext cc)
+      throws ODKEntityNotFoundException, ODKOverQuotaException {
     String value = getServerPreferencesProperty(cc, FASTER_BACKGROUND_ACTIONS_DISABLED);
-    if ( value != null ) {
+    if (value != null) {
       return Boolean.valueOf(value);
     }
     // null value should be treated as false
     return false;
   }
 
-  public static void setFasterBackgroundActionsDisabled(CallingContext cc, Boolean disabled) throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static void setFasterBackgroundActionsDisabled(CallingContext cc, Boolean disabled)
+      throws ODKEntityNotFoundException, ODKOverQuotaException {
     setServerPreferencesProperty(cc, FASTER_BACKGROUND_ACTIONS_DISABLED, disabled.toString());
   }
 
@@ -214,9 +261,11 @@ public class ServerPreferencesProperties extends CommonFieldsBase {
       throws ODKEntityNotFoundException, ODKOverQuotaException {
     try {
       ServerPreferencesProperties relation = assertRelation(cc);
-      Query query = cc.getDatastore().createQuery(relation, "ServerPreferences.getServerPreferences", cc.getCurrentUser());
+      Query query = cc.getDatastore().createQuery(relation,
+          "ServerPreferences.getServerPreferences", cc.getCurrentUser());
       query.addFilter(KEY, Query.FilterOperation.EQUAL, keyName);
-      // don't care about duplicate entries because we always access the most recent first
+      // don't care about duplicate entries because we always access the most
+      // recent first
       query.addSort(relation.lastUpdateDate, Query.Direction.DESCENDING);
       List<? extends CommonFieldsBase> results = query.executeQuery();
       if (!results.isEmpty()) {
@@ -233,18 +282,20 @@ public class ServerPreferencesProperties extends CommonFieldsBase {
     }
   }
 
-  public static final void setServerPreferencesProperty(CallingContext cc, String keyName, String value)
-      throws ODKEntityNotFoundException, ODKOverQuotaException {
+  public static final void setServerPreferencesProperty(CallingContext cc, String keyName,
+      String value) throws ODKEntityNotFoundException, ODKOverQuotaException {
     try {
       ServerPreferencesProperties relation = assertRelation(cc);
-      Query query = cc.getDatastore().createQuery(relation, "ServerPreferences.getServerPreferences", cc.getCurrentUser());
+      Query query = cc.getDatastore().createQuery(relation,
+          "ServerPreferences.getServerPreferences", cc.getCurrentUser());
       query.addFilter(KEY, Query.FilterOperation.EQUAL, keyName);
       List<? extends CommonFieldsBase> results = query.executeQuery();
       if (!results.isEmpty()) {
         if (results.get(0) instanceof ServerPreferencesProperties) {
           ServerPreferencesProperties preferences = (ServerPreferencesProperties) results.get(0);
-          if ( !preferences.setStringField(VALUE, value) ) {
-            throw new IllegalStateException("Unexpected truncation of ServerPreferencesProperty: " + keyName + " value");
+          if (!preferences.setStringField(VALUE, value)) {
+            throw new IllegalStateException("Unexpected truncation of ServerPreferencesProperty: "
+                + keyName + " value");
           }
           preferences.persist(cc);
           return;
@@ -252,13 +303,15 @@ public class ServerPreferencesProperties extends CommonFieldsBase {
         throw new IllegalStateException("Expected ServerPreferencesProperties entity");
       }
       // nothing there -- put the value...
-      ServerPreferencesProperties preferences = cc.getDatastore().createEntityUsingRelation(relation,
-                                                                          cc.getCurrentUser());
-      if ( !preferences.setStringField(KEY, keyName) ) {
-        throw new IllegalStateException("Unexpected truncation of ServerPreferencesProperty: " + keyName + " keyName");
+      ServerPreferencesProperties preferences = cc.getDatastore().createEntityUsingRelation(
+          relation, cc.getCurrentUser());
+      if (!preferences.setStringField(KEY, keyName)) {
+        throw new IllegalStateException("Unexpected truncation of ServerPreferencesProperty: "
+            + keyName + " keyName");
       }
-      if ( !preferences.setStringField(VALUE, value) ) {
-        throw new IllegalStateException("Unexpected truncation of ServerPreferencesProperty: " + keyName + " value");
+      if (!preferences.setStringField(VALUE, value)) {
+        throw new IllegalStateException("Unexpected truncation of ServerPreferencesProperty: "
+            + keyName + " value");
       }
       preferences.persist(cc);
       return;
