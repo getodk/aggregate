a4adef5f88cf03d16ecd371b8338221b4e1fa02e
mitchellsundt <mitchellsundt@gmail.com>
2014-11-17 17:19:35 -0800

    Add guards to only resize elements if their sizes change, and to not resize if a resize is already underway. Update to jQuery 1.11.1 Change plumbing of redirects slightly.

--- eclipse-aggregate-gae/war/javascript/main.js
+++ eclipse-aggregate-gae/war/javascript/main.js
@@ -4,6 +4,11 @@ var contentLoaded = false;
 function gwtContentLoaded() {
 	// Set up window resize handler and call it.
 	contentLoaded = true;
+}
+
+
+//Runs before gwt is finished putting elements on the page
+function gwtBeforeContentLoaded() {
+	// Set up window resize handler
 	$(window).resize(onWindowResize);
-	onWindowResize();
 }
