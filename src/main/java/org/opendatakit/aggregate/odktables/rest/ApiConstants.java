10b9a30bd779f7ed96d3a9ed58b49ca060884266
Mitch Sundt <mitchellsundt@gmail.com>
2016-05-09 11:14:47 -0700

    Updated header values in support of migration to Apache 4.5.2 on clients.

--- src/main/java/org/opendatakit/aggregate/odktables/rest/ApiConstants.java
+++ src/main/java/org/opendatakit/aggregate/odktables/rest/ApiConstants.java
@@ -16,6 +16,13 @@
 
 package org.opendatakit.aggregate.odktables.rest;
 
+/**
+ * HTTP Header fields and values for those fields. Some of these are here so 
+ * as to avoid needing to use Android-specific classes to reference them.
+ * 
+ * @author mitchellsundt@gmail.com
+ *
+ */
 public final class ApiConstants {
 
   private ApiConstants() {
@@ -27,17 +34,31 @@ public final class ApiConstants {
 
   public static final String DATE_HEADER = "Date";
 
+  public static final String ACCEPT_HEADER = "Accept";
+
+  public static final String ACCEPT_CHARSET_HEADER = "Accept-Charset";
+
   public static final String UTF8_ENCODE = "UTF-8";
 
-  public static final String MEDIA_TEXT_XML_UTF8 = "text/xml;charset=UTF-8";
-
-  public static final String MEDIA_APPLICATION_XML_UTF8 = "application/xml;charset=UTF-8";
-
   public static final String ACCEPT_CONTENT_ENCODING_HEADER = "Accept-Encoding";
+
+  public static final String ACCEPT_LANGUAGE_HEADER = "Accept-Language";
+
+  public static final String AUTHORIZATION_HEADER = "Authorization";
 
   public static final String CONTENT_ENCODING_HEADER = "Content-Encoding";
 
   public static final String GZIP_CONTENT_ENCODING = "gzip";
 
   public static final String DEFLATE_CONTENT_ENCODING = "deflate";
+
+  public static final String CONTENT_TYPE_HEADER = "Content-Type";
+
+  public static final String MEDIA_TEXT_XML_UTF8 = "text/xml;charset=UTF-8";
+
+  public static final String MEDIA_APPLICATION_XML_UTF8 = "application/xml;charset=UTF-8";
+  
+  public static final String ETAG_HEADER = "ETag";
+  
+  public static final String LOCATION_HEADER = "Location";
 }
