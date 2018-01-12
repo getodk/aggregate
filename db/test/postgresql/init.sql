42cfcbb9ebfd9b333ad1b4e8161162a1957f620e
Yaw Anokwa <yanokwa@nafundi.com>
2018-01-11 16:22:10 -0800

    Enable testing on CircleCI

---
+++ db/test/postgresql/init.sql
@@ -0,0 +1,6 @@
+CREATE USER "odk_unit" WITH UNENCRYPTED PASSWORD 'test';
+CREATE DATABASE odk_db WITH OWNER odk_unit;
+GRANT ALL PRIVILEGES ON DATABASE odk_db TO odk_unit;
+\connect odk_db;
+CREATE SCHEMA odk_db;
+GRANT ALL PRIVILEGES ON SCHEMA odk_db TO odk_unit;
