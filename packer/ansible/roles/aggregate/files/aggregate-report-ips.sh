fe204d066284ba099297d96013067860e8f3fe1c
Guille <ggalmazor@gmail.com>
2018-03-15 17:01:42 +0100

    Close #219 - Improve UX message on VM startup

--- packer/ansible/roles/aggregate/files/aggregate-report-ips.sh
+++ packer/ansible/roles/aggregate/files/aggregate-report-ips.sh
@@ -5,21 +5,21 @@ PROPS_FILE=/var/lib/tomcat8/webapps/ROOT/WEB-INF/classes/security.properties
 rawHostname=$(grep hostname ${PROPS_FILE} | awk '{split($0,a,"="); print a[2]}')
 hostname=${rawHostname:-localhost}
 httpPort=$(grep port ${PROPS_FILE} | awk '{split($0,a,"="); print a[2]}')
-httpsPort=$(grep securePort ${PROPS_FILE} | awk '{split($0,a,"="); print a[2]}')
+ips=$(ip address | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | awk -F'/' '{print $1}')
+VERSION=cocotero
 
-echo "Open a browser and use one of these URLs to access ODK Aggregate"
+echo "> Welcome to ODK Aggregate VM $VERSION"
+echo "> 1. Open the web browser on your computer"
 if [ ${httpPort} = "80" ]; then
-  echo "http://${hostname}"
+  echo "> 2. Go to http://${hostname}"
 else
-  echo "http://${hostname}:${httpPort}"
+  echo "> 2. Go to http://${hostname}:${httpPort}"
 fi
-if [ ${httpsPort} = "443" ]; then
-  echo "https://${hostname}"
-else
-  echo "https://${hostname}:${httpsPort}"
-fi
-echo ""
+echo "> 3. Sign in with the Aggregate password"
+echo "> Need the password? Read the readme.txt file."
 
-echo "This VM uses these IP addresses:"
-ip address | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | awk -F'/' '{print $1}'
-echo ""
+if [ ! -z ${ips} ]; then
+  echo ">"
+  echo "> If the above URL does not work, these IP addresses instead"
+  echo "${ips}" | while IFS= read -r line ; do echo "> ${line}"; done
+fi
