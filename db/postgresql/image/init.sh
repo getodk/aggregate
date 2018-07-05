ee8dfdce41c0fa122ca7e1ef1f9149beac096a56
Guille <ggalmazor@gmail.com>
2018-07-05 18:09:25 +0200

    Fix dev db init scripts use secure passwords - We were using UNENCRYPTED PASSWORD, which is insecure and won't be supported from PostgreSQL 10 onwards.

--- db/postgresql/image/init.sh
+++ db/postgresql/image/init.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER "odk_unit" WITH UNENCRYPTED PASSWORD 'test';
+    CREATE USER "odk_unit" WITH PASSWORD 'test';
     CREATE DATABASE odk_db WITH OWNER odk_unit;
     GRANT ALL PRIVILEGES ON DATABASE odk_db TO odk_unit;
     \connect odk_db;
--- db/test/postgresql/init.sql
+++ db/test/postgresql/init.sql
@@ -1,4 +1,4 @@
-CREATE USER "odk_unit" WITH UNENCRYPTED PASSWORD 'test';
+CREATE USER "odk_unit" WITH PASSWORD 'test';
 CREATE DATABASE odk_db WITH OWNER odk_unit;
 GRANT ALL PRIVILEGES ON DATABASE odk_db TO odk_unit;
 \connect odk_db;
