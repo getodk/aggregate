bfe89cc292cda83862aba7ae43827b713b94a69a
Guille <ggalmazor@gmail.com>
2018-08-17 10:27:50 +0200

    Add doc blocks explaining the new secure request methods

--- src/main/java/org/opendatakit/aggregate/client/security/SecurityUtils.java
+++ src/main/java/org/opendatakit/aggregate/client/security/SecurityUtils.java
@@ -17,10 +17,27 @@ public class SecurityUtils {
     void apply(T rpc, String sessionCookie, AsyncCallback<U> callback);
   }
 
+  /**
+   * Executes a secure request (with CSRF protection) to make a RPC.
+   * <p>
+   * This overload is used when the RPC call doesn't return anything (Void type)
+   *
+   * @see #secureRequest(Object, SecureRpcRequest, Consumer, Consumer)
+   */
   public static <T> void secureRequest(T rpc, SecureRpcRequest<T, Void> request, Runnable onSuccess, Consumer<Throwable> onFailure) {
     secureRequest(rpc, request, __ -> onSuccess.run(), onFailure);
   }
 
+  /**
+   * Executes a secure request (with CSRF protection) to make a RPC.
+   *
+   * @param rpc       RPC class that will be invoked
+   * @param request   Request that makes the actual RPC call
+   * @param onSuccess Callback to be called on success. It receives the request's output
+   * @param onFailure Callback to be called on failure. It receives the {@link Throwable} cause of the failure
+   * @param <T>       Type of the RPC class
+   * @param <U>       Type of the output of the RPC call
+   */
   public static <T, U> void secureRequest(T rpc, SecureRpcRequest<T, U> request, Consumer<U> onSuccess, Consumer<Throwable> onFailure) {
     XsrfTokenServiceAsync xsrf = GWT.create(XsrfTokenService.class);
     ((ServiceDefTarget) xsrf).setServiceEntryPoint(GWT.getModuleBaseURL() + "xsrf");
