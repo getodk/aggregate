87ae8848a1b254602cb7d1a777a9f72456bb2b08
Guille <ggalmazor@gmail.com>
2018-09-27 13:53:09 +0200

    Apply fixed suggested by shellcheck

--- packer/ansible/roles/aggregate/files/aggregate-config.sh
+++ packer/ansible/roles/aggregate/files/aggregate-config.sh
@@ -58,26 +58,26 @@ showHelp() {
   echo "--net-mode   <value> Set the VM's network mode. Use 'nat' or 'bridge'"
 }
 
-if [ ${HELP} = YES ]; then
+if [ "${HELP}" = YES ]; then
   showHelp
   exit 0
 fi
 
-if [ ${NET_MODE} != "nat" ] && [ ${NET_MODE} != "bridge" ]; then
+if [ "${NET_MODE}" != "nat" ] && [ "${NET_MODE}" != "bridge" ]; then
   echo "Error: Invalid --net-mode value. Use 'nat' or 'bridge' according to the VM's networking configuration"
   echo ""
   showHelp
   exit 1
 fi
 
-if [ ${NET_MODE} == "bridge" ] && ([ -z ${HTTP_PORT} ] || [ -z ${HTTPS_PORT} ]); then
+if [ "${NET_MODE}" == "bridge" ] && ([ -z "${HTTP_PORT}" ] || [ -z "${HTTPS_PORT}" ]); then
   echo "Error: In net mode 'bridge' you need to provide values for --http-port and --https-port"
   echo ""
   showHelp
   exit 1
 fi
 
-if [ ${HELP} = NO ] && [ -z ${FQDN} ] && [ -z ${HTTP_PORT} ] && [ -z ${HTTPS_PORT} ]; then
+if [ "${HELP}" = NO ] && [ -z "${FQDN}" ] && [ -z "${HTTP_PORT}" ] && [ -z "${HTTPS_PORT}" ]; then
   echo "Error: You need to set at least one argument"
   echo ""
   showHelp
@@ -88,33 +88,33 @@ echo "Stopping Tomcat. Please wait..."
 echo ""
 service tomcat8 stop
 
-if [ ! -z ${FQDN} ] && [ ${FQDN} = "auto" ]; then
-  cat ${PROPS_FILE} | sed -e "s/^security\.server\.hostname=.*$/security.server.hostname=/" > /tmp/temp_file
+if [ ! -z "${FQDN}" ] && [ "${FQDN}" = "auto" ]; then
+  sed -e "s/^security\.server\.hostname=.*$/security.server.hostname=/" ${PROPS_FILE} > /tmp/temp_file
   cp /tmp/temp_file ${PROPS_FILE}
 fi
 
-if [ ! -z ${FQDN} ] && [ ! ${FQDN} = "auto" ]; then
-  cat ${PROPS_FILE} | sed -e "s/^security\.server\.hostname=.*$/security.server.hostname=${FQDN}/" > /tmp/temp_file
+if [ ! -z "${FQDN}" ] && [ ! "${FQDN}" = "auto" ]; then
+  sed -e "s/^security\.server\.hostname=.*$/security.server.hostname=${FQDN}/" ${PROPS_FILE} > /tmp/temp_file
   cp /tmp/temp_file ${PROPS_FILE}
 fi
 
-if [ ! -z ${HTTP_PORT} ]; then
-  cat ${PROPS_FILE} | sed -e "s/^security\.server\.port=.*$/security.server.port=${HTTP_PORT}/" > /tmp/temp_file
+if [ ! -z "${HTTP_PORT}" ]; then
+  sed -e "s/^security\.server\.port=.*$/security.server.port=${HTTP_PORT}/" ${PROPS_FILE} > /tmp/temp_file
   cp /tmp/temp_file ${PROPS_FILE}
 fi
 
-if [ ! -z ${HTTPS_PORT} ]; then
-  cat ${PROPS_FILE} | sed -e "s/^security\.server\.securePort=.*$/security.server.securePort=${HTTPS_PORT}/" > /tmp/temp_file
+if [ ! -z "${HTTPS_PORT}" ]; then
+  sed -e "s/^security\.server\.securePort=.*$/security.server.securePort=${HTTPS_PORT}/" ${PROPS_FILE} > /tmp/temp_file
   cp /tmp/temp_file ${PROPS_FILE}
 fi
 
-if [ ${NET_MODE} = "nat" ]; then
-  cat ${TOMCAT_CONFIG_FILE} | sed -e "s/^.*Connector.*$/    <Connector port=\"80\" protocol=\"HTTP\/1.1\" connectionTimeout=\"20000\" redirectPort=\"443\"\/>/" > /tmp/temp_file
+if [ "${NET_MODE}" = "nat" ]; then
+  sed -e "s/^.*Connector.*$/    <Connector port=\"80\" protocol=\"HTTP\/1.1\" connectionTimeout=\"20000\" redirectPort=\"443\"\/>/" ${TOMCAT_CONFIG_FILE} > /tmp/temp_file
   cp /tmp/temp_file ${TOMCAT_CONFIG_FILE}
 fi
 
-if [ ${NET_MODE} = "bridge" ]; then
-  cat ${TOMCAT_CONFIG_FILE} | sed -e "s/^.*Connector.*$/    <Connector port=\"${HTTP_PORT}\" protocol=\"HTTP\/1.1\" connectionTimeout=\"20000\" redirectPort=\"${HTTPS_PORT}\"\/>/" > /tmp/temp_file
+if [ "${NET_MODE}" = "bridge" ]; then
+  sed -e "s/^.*Connector.*$/    <Connector port=\"${HTTP_PORT}\" protocol=\"HTTP\/1.1\" connectionTimeout=\"20000\" redirectPort=\"${HTTPS_PORT}\"\/>/" ${TOMCAT_CONFIG_FILE} > /tmp/temp_file
   cp /tmp/temp_file ${TOMCAT_CONFIG_FILE}
 fi
 
