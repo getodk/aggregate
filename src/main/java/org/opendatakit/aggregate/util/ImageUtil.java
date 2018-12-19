5d67faf45a4dc329a833c7ceb9fc1cb355e683d6
Guille <ggalmazor@gmail.com>
2018-12-19 13:57:08 +0100

    Unroll Tomcat Util dependency

--- src/main/java/org/opendatakit/aggregate/util/ImageUtil.java
+++ src/main/java/org/opendatakit/aggregate/util/ImageUtil.java
@@ -16,13 +16,67 @@
 
 package org.opendatakit.aggregate.util;
 
-import org.opendatakit.tomcatutil.ImageResize;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class ImageUtil {
 
   public byte[] resizeImage(byte[] imageBlob, int width, int height) {
-    ImageResize resize = new ImageResize();
-    return resize.resizeImage(imageBlob, width, height);
+    BufferedImage image;
+    try {
+      image = ImageIO.read(new ByteArrayInputStream(imageBlob));
+    } catch (IOException var18) {
+      var18.printStackTrace();
+      return imageBlob;
+    }
+
+    int imgWidth = image.getWidth();
+    int imgHeight = image.getHeight();
+    if (width > 0 && height > 0) {
+      int reductionWidth = imgWidth / width;
+      int reductionHeight = imgHeight / height;
+      if (reductionWidth > 0 && reductionHeight > 0) {
+        int reducer;
+        if (reductionWidth > reductionHeight) {
+          reducer = reductionWidth;
+          if (imgWidth % width != 0) {
+            reducer = reductionWidth + 1;
+          }
+        } else {
+          reducer = reductionHeight;
+          if (imgHeight % height != 0) {
+            reducer = reductionHeight + 1;
+          }
+        }
+
+        int resizeWidth = imgWidth / reducer;
+        int resizeHeight = imgHeight / reducer;
+        Image resized = image.getScaledInstance(resizeWidth, resizeHeight, 2);
+        ByteArrayOutputStream fileStream = new ByteArrayOutputStream();
+        BufferedImage bi = new BufferedImage(resized.getWidth(null), resized.getHeight(null), 1);
+        Graphics bg = bi.getGraphics();
+        bg.drawImage(resized, 0, 0, null);
+        bg.dispose();
+
+        try {
+          ImageIO.write(bi, "jpg", fileStream);
+        } catch (IOException var17) {
+          var17.printStackTrace();
+          return imageBlob;
+        }
+
+        return fileStream.toByteArray();
+      } else {
+        return imageBlob;
+      }
+    } else {
+      return imageBlob;
+    }
   }
 
 }
