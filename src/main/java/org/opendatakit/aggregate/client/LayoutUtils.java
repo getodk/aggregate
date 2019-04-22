0f9823d844282f0ec5b3ba71838e8868f2d8975d
Guille <ggalmazor@gmail.com>
2019-04-22 10:32:05 +0200

    Replace the client's implementation of the version note - We now use a new service that will return the full version note depending on the current deployed version and the latest available version as reported by GitHub (which refreshes every 24 hours) - The main reason for this change is that Aggregate should be able to load without access to the internet. - This required changing the XMLHttpRequest to run asynchronously and subscribing to the error events, but this conflicted with the blocking nature of GWT Java UI building and the complexities of JSNI interaction. - By moving the interaction with GitHub to the server, we can simplify the client code (no JSNI, no asynchronous code issues).

--- src/main/java/org/opendatakit/aggregate/client/LayoutUtils.java
+++ src/main/java/org/opendatakit/aggregate/client/LayoutUtils.java
@@ -3,28 +3,14 @@ package org.opendatakit.aggregate.client;
 import static org.opendatakit.aggregate.buildconfig.BuildConfig.VERSION;
 
 import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.UIObject;
 
 public class LayoutUtils {
-  private static String latestVersion;
-
-  static native String getLatestVersion() /*-{
-    var req = new XMLHttpRequest();
-    req.open('GET', 'https://api.github.com/repos/opendatakit/aggregate/releases/latest', false);
-    req.send(null);
-    if (req.readyState === 4 && req.status === 200)
-      return JSON.parse(req.responseText).tag_name;
-  }-*/;
-
   static HTML buildVersionNote(UIObject parent) {
-    if (latestVersion == null)
-      latestVersion = getLatestVersion();
     String shortVersion = VERSION.contains("-") ? VERSION.substring(0, VERSION.indexOf("-")) : VERSION;
-    String versionNote = shortVersion.equals(latestVersion)
-        ? "You're up to date!"
-        : "<a href=\"https://github.com/opendatakit/aggregate/releases/latest\" target=\"_blank\">Update available</a>";
-    HTML html = new HTML("<small>" + shortVersion + " - " + versionNote + "</small>");
+    HTML html = new HTML("<small>" + shortVersion + " - Checking versions...</small>");
     Style style = html.getElement().getStyle();
     style.setProperty("position", "fixed");
     style.setProperty("bottom", "0");
@@ -34,6 +20,18 @@ public class LayoutUtils {
 
     Style parentStyle = parent.getElement().getStyle();
     parentStyle.setProperty("paddingBottom", "40px");
+
+    SecureGWT.getPreferenceService().getVersioNote(new AsyncCallback<String>() {
+      @Override
+      public void onFailure(Throwable caught) {
+        html.setHTML("<small>" + shortVersion + " - Version check failed</small>");
+      }
+
+      @Override
+      public void onSuccess(String versionNote) {
+        html.setHTML("<small>" + versionNote + "</small>");
+      }
+    });
 
     return html;
   }
